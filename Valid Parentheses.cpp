f6c36a043c9c9c31655e67cf41a82a180b77c608
Jean <j.z.feng@foxmail.com>
2015-09-04 15:53:36 +0800

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+	bool isValid(string s){
+		stack<char> stk;
+		int len=s.length();
+
+		for (int i=0;i<len;i++){
+			if (s[i]=='['||s[i]=='{'||s[i]=='('){
+				stk.push(s[i]);
+			}
+			else {
+				if (stk.empty()){
+					return false;
+				}
+				else if (stk.top()=='['&&s[i]==']'){
+					stk.pop();
+				}
+				else if (stk.top()=='{'&&s[i]=='}'){
+					stk.pop();
+				}
+				else if (stk.top()=='('&&s[i]==')'){
+					stk.pop();
+				}
+				else{
+					return false;
+				}
+			}
+		}
+		return stk.empty();
+	}
+protected:
+private:
+};
