29ce0c9558b12f91285ad89b1ed657364b13d9a8
Jean <j.z.feng@foxmail.com>
2015-09-05 13:47:51 +0800

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -1,49 +1,32 @@
-#include "iostream"
-#include "cstdio"
-#include "cstring"
-#include "stack"
-#include "algorithm"
-
-using namespace std;
-
 class Solution
 {
 public:
 	int longestValidParentheses(string s){
-		stack<int> st;
+		stack<int> stk;
 		int result=0,accu=0;
-
 		for (int i=0;i<s.size();i++){
 			if (s[i]=='('){
-				st.push(i);
+				stk.push(i);//存放不能配对的索引。
 			}
 			else{
-				if (st.empty()){
-					accu=0;
+				if (stk.empty()){
+					accu=0;//重新确定初始位置
 				}
-				else {
-					int j=st.top();
-					st.pop();
-					if (st.empty()){
+				else{
+					int j=stk.top();
+					stk.pop();
+					if(stk.empty()){
 						accu+=i-j+1;
-						result=max(accu,result);
+						result=max(result,accu);
 					}
-					else {
-						result=max(i-j+1,result);
-					}
+					else
+						result=max(result,i-stk.top());
 				}
 			}
 		}
-	return result;
-	}
+		return result;
+	} 
+
+protected:
+private:
 };
-
-
-int main(){
-	string test;
-	Solution s;
-	while (cin>>test){
-		cout<<s.longestValidParentheses(test)<<endl;
-	}
-}
-
