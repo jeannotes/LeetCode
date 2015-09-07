5a96e0a7b3ef7cce5e6d78dc27f640a526a8e19c
Jean <j.z.feng@foxmail.com>
2015-09-08 00:12:14 +0800

    Update Pascal's Triangle.cpp

--- Pascal's Triangle.cpp
+++ Pascal's Triangle.cpp
@@ -1,21 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> generate(int numRows) {
-		vector < vector<int> > triangle;
-		for (int i=0;i<numRows;i++){
+	vector< vector< int > > generate(int numRows){
+		vector< vector <int > >triangle;
+		for (int i = 0; i < numRows; i++)
+		{
 			vector<int> v;
-			if (i==0){
+			if (i==0)
+			{
 				v.push_back(1);
 			}
-			else{
+			else
+			{
 				v.push_back(1);
-				for (int j=1;j<i;j++){
+				for (int j = 1; j <= i-1; j++)
+				{
 					v.push_back(triangle[i-1][j]+triangle[i-1][j-1]);
 				}
 				v.push_back(1);
 			}
 			triangle.push_back(v);
 		}
+		
 		return triangle;
-    }
+	}
 };
+
+//第二次编写，还是一些c++语法不太懂，基础很重要，不过，我认为这些无所谓。
+//后期我会用c重新编写。
