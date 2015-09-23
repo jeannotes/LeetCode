7c43b666cd8001b16220ba45a46ab965d4948426
Jean <j.z.feng@foxmail.com>
2015-09-24 00:51:34 +0800

    Create Binary Tree Inorder Traversal.cpp

---
+++ Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ //https://leetcode.com/problems/binary-tree-inorder-traversal/
+class Solution1 {
+public:
+    vector<int> inorderTraversal1(TreeNode* root){
+        vector<int> result;
+        inorderHelper(root,result);
+        return result;
+    }
+    
+    void inorderHelper(TreeNode *root,vector<int> &result){
+        if(root==NULL) return;
+        inorderHelper(root->left,result);
+        result.push_back(root->val);
+        inorderHelper(root->right,result);
+    }
+};
+
+
+class Solution2 {
+public:
+    vector<int> inorderTraversal2(TreeNode* root){
+        stack<TreeNode *>st;
+        vector<int> result;
+        
+        while(!(root==NULL&&st.empty())){
+            if(root!=NULL){
+                st.push(root);
+                root=root->left;
+            }
+            else{
+                root=st.top();st.pop();
+                result.push_back(root->val);
+                root=root->right;
+            }
+            //这个时候while循环是不对的，如果只有左子树，就不能出结果。因为有堆栈，可以判断是否为空。
+        }
+        return result;
+    }
+    
+};
