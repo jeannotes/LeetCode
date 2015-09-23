338977ed97c7fb1503cce4798ed1f639637e95b6
Jean <j.z.feng@foxmail.com>
2015-09-24 00:49:22 +0800

    Create Binary Tree Postorder Traversal.cpp

---
+++ Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+//http://blog.csdn.net/jsrgfjz/article/details/48696527
+//https://leetcode.com/problems/binary-tree-postorder-traversal/
+    vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> result;
+    vector<TreeNode*> stack;
+    TreeNode *node=root;
+    TreeNode *lastVisitNode=NULL;
+    
+    while(node!=NULL||stack.size()>0){
+        if(node!=NULL){
+            stack.push_back(node);
+            node=node->left;
+        }else{
+            TreeNode *n=stack.back();
+            if(n->right==NULL||lastVisitNode==n->right){
+                //如果为空，访问根节点，如刚刚访问右节点，访问根节点。
+                result.push_back(n->val);
+                stack.pop_back();
+                lastVisitNode=n;
+            }
+            else{
+                //访问的左节点,没有关系，反正最后还是会转移到这个节点
+                node=n->right;
+            }
+        }
+    }
+    return result;
+}
+};
