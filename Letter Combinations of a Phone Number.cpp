5f645add0cfea3b0c4a6d1f688c8b19953c6cb29
Jean <j.z.feng@foxmail.com>
2015-10-08 20:16:07 +0800

    Create Letter Combinations of a Phone Number.cpp

---
+++ Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+    vector<string> letterCombinations(string digits) {
+        string table[]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};//  (number:2-9)==(0-7)
+        vector<string> result;
+        if(digits.empty()){     //考虑输入为空的情况
+            return result;   
+        }
+        string s;//缓存
+        combinationHelper(table,digits,0,s,result);//table[]不对
+        return result;
+    }
+    
+    void combinationHelper(string table[],string &digits,int start,string &s,vector<string> &result){
+        if(start==digits.size()){
+            result.push_back(s);
+            return;
+        }//限制条件
+        
+        int i=digits[start]-'2';
+        if(i<0||i>7){
+            return;//本来以为这句是没啥用的，只是后面start不断+1,会导致溢出，所以有用。
+        }
+        
+        for(int j=0;j<table[i].size();j++){
+            s.push_back(table[i][j]);
+            combinationHelper(table,digits,start+1,s,result);
+            s.pop_back();
+        }
+    }
+};
