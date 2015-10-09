d64df250be9d50f31a64c500d81c244c960841bd
Jean <j.z.feng@foxmail.com>
2015-10-10 00:44:05 +0800

    Update Best Time to Buy and Sell Stock.cpp

--- Best Time to Buy and Sell Stock.cpp
+++ Best Time to Buy and Sell Stock.cpp
@@ -1,5 +1,5 @@
 //本题只能买卖1次，从前到后
-//一定还有动态规划的解法
+//写出了两种解法。
 class Solution1 {
 public:
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
@@ -21,3 +21,25 @@ public:
     }
 };
 
+ class Solution2 {
+ public:
+     int maxProfit(vector<int> &prices) {
+         int length=prices.size();
+         if(length<=1){
+             return 0;
+         }
+         vector<int> dp(length,0);
+         int min=prices[0];
+         for(int i=1;i<length;i++){
+             if(prices[i]<=min){
+                 min=prices[i];
+             }
+             dp[i]=max(  dp[i-1],prices[i]-min   );
+         }
+         sort(dp.begin(),dp.end());
+         return dp.back();
+     }
+ };
+ 
+ //dp[i] 表示[0...i]内最大值，则dp[i+1]=max(dp[i],prices[i+1]-min)
+ //min是[0...i]内的最小值
