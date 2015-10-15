1edb0bb94e4993735b8bc65895099e1684ca4a4c
Jean <j.z.feng@foxmail.com>
2015-10-15 19:24:16 +0800

    Create Path Sum II.cpp

---
+++ Path Sum II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+//https://leetcode.com/problems/path-sum-ii/
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<int> temp;
+        vector< vector<int> >result;
+        pathSumHelper(root,sum,temp,result);
+        return result;
+        //好久时间
+    }
+    
+    void pathSumHelper(TreeNode *root,int sum,vector<int> &temp,vector<vector<int> >&result){
+        if(root==NULL){
+            return;
+        }
+        temp.push_back(root->val);
+        if(root->left==NULL&&root->right==NULL&&root->val==sum){
+            result.push_back(temp);
+        }
+        pathSumHelper(root->left,sum - root->val,temp,result);
+        pathSumHelper(root->right,sum - root->val,temp,result);
+        
+        temp.pop_back();
+    }
+};
