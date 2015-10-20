58a7233a3d5c1d2900fef08a81d5d0df1d4579dd
Jean <j.z.feng@foxmail.com>
2015-10-20 19:56:03 +0800

    Create Palindrome Partitioning II.cpp

---
+++ Palindrome Partitioning II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+//https://leetcode.com/problems/palindrome-partitioning-ii/
+    int minCut(string s) {
+        //核心公式
+        //  count[i]=从i到尾的最小剪切数
+        //  count[i]=min{  count[j+1]+1  }  +  dp[i][j]==1   j=i....s.length()        
+        int len = s.size();  
+        int count[len+1];
+        bool dp[len][len];     
+            
+        for(int i = 0; i <= len; i++){
+            count[i] = len-i;     
+        }   
+                 
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                dp[i][j] = false;     
+            }   
+        }  
+
+
+        for(int i=len-1;i>=0;i--){
+            
+            for(int j=i;j<=len-1;j++){
+                if((j-i<2||dp[i+1][j-1]==1)&& s[i]==s[j]){
+                    dp[i][j]=true;
+                    count[i]=min(   count[i],count[j+1]+1    );
+                }
+            }
+        }
+        
+        return count[0]-1;
+    }
+};
