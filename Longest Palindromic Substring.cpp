68cc2d4b29d9e4d8ed082651c9e009db0b6fe4c2
Jean <j.z.feng@foxmail.com>
2015-10-27 08:15:36 +0800

    Update Longest Palindromic Substring.cpp

--- Longest Palindromic Substring.cpp
+++ Longest Palindromic Substring.cpp
@@ -3,34 +3,43 @@ public:
 //在之前有一个题目使用自下而上的解法
 //http://articles.leetcode.com/2011/11/longest-palindromic-substring-part-i.html
 //这只是第一种想法，还有一种待会写。
-
-    string expandAroundCenter(string s, int c1, int c2) {
-  int l = c1, r = c2;
-  int n = s.length();
-  while (l >= 0 && r <= n-1 && s[l] == s[r]) {
-    l--;
-    r++;
-  }
-  return s.substr(l+1, r-l-1);
-}
- 
-string longestPalindrome(string s) {
-  int n = s.length();
-  if (n == 0) return "";
-  string longest = s.substr(0, 1);  // a single char itself is a palindrome
-  for (int i = 0; i < n-1; i++) {
-    string p1 = expandAroundCenter(s, i, i);
-    if (p1.length() > longest.length())
-      longest = p1;
- 
-    string p2 = expandAroundCenter(s, i, i+1);
-    if (p2.length() > longest.length())
-      longest = p2;
-  }
-  return longest;
-}
+    string longestPalindrome(string s){
+        int n=s.length();
+        int longestBegin=0;
+        int maxLen=1;
+        bool table[1000][1000]={false};//  s[i....j]
+        
+        for(int i=0;i<n;i++){
+            table[i][i]=true;//单个字符绝对可以成为回文串
+            longestBegin=i;
+            maxLen=1;
+        }
+        
+        //table的初始化
+        for(int i=0;i<=n-2;i++){
+            if(s[i]==s[i+1]){//看有没有相隔2个字符可以成为回文串的
+                table[i][i+1]=true;
+                longestBegin=i;
+                maxLen=2;
+            }
+        }
+        //接下来查看间隔是3的情形
+        for(int len=3;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=len+i-1;
+                if(s[i]==s[j]&&table[i+1][j-1]){
+                    table[i][j]=true;
+                    longestBegin=i;
+                    maxLen=len;
+                }
+            }
+        }
+        
+        return s.substr(longestBegin,maxLen);
+    }
 
 };
+
 
 class Solution2 {
 public:
