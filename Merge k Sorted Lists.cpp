6cb067cdc4811400dd685bf82f1a302d528169c8
Jean <j.z.feng@foxmail.com>
2015-11-04 00:38:13 +0800

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    class Comp{
+        public:
+        bool operator()(const ListNode *l1,ListNode *l2){
+            return l1->val>l2->val;
+        }
+    };
+
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if(lists.empty())   return NULL;
+        
+        ListNode *dummy=new ListNode(0),*tail=dummy;
+        priority_queue<ListNode *,vector<ListNode *>,Comp>pq;
+        
+        for(int i=0;i<lists.size();i++){
+            if(lists[i])    pq.push(lists[i]);
+        }
+        
+        while(!pq.empty()){
+            ListNode *tmp=pq.top();pq.pop();
+            if(tmp->next)   pq.push(tmp->next);
+            tail->next=tmp;
+            tail=tail->next;
+        }
+        //pq优先队列里面已经是最大堆，每一个链表的第一个数都是按照最大的顺序排列。
+        ListNode *head=dummy->next;
+        delete dummy;
+        return head;
+    }
+};
