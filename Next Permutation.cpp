3238e0a1c127c386da9babe988c3ee60f8bd1fb7
Jean <j.z.feng@foxmail.com>
2015-11-07 23:58:44 +0800

    Update and rename next-permutation.cpp to Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i=nums.size()-2;
+        while(i>=0&&nums[i]>=nums[i+1])   i--;//此时的i代表第一个比下面一个数小的index
+        
+        if(i>=0){
+            //说明可以比较
+            int j=i+1;
+            while(j<=nums.size()-1&&nums[j]>nums[i]){
+                j++;
+            }
+            j--;
+            swap(nums[i],nums[j]);
+        }
+        reverse(nums,i+1);
+        
+    }
+    
+    void reverse(vector<int>& nums,int index){
+        int start=index,end=nums.size()-1;
+        while(start<=end){
+            swap(nums[start],nums[end]);start++;end--;
+        }
+    }
+};
