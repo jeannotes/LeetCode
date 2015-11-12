69d1b14d8803ad0416513a7c487d8c1ded5db063
Jean <j.z.feng@foxmail.com>
2015-11-12 08:30:11 +0800

    Create Length of Last Word.cpp

---
+++ Length of Last Word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+//https://leetcode.com/problems/length-of-last-word/
+public:
+    int lengthOfLastWord(string s) {
+        if(s.empty())   return 0;
+        //从后往前找第一个非" "的字符，如果没有，返回0
+        int len=s.length();
+        int j=len-1;
+        while(j>=0&&s[j]==' ')   j--;
+        if(j<0) return 0;
+        for(int i=j;i>=0;i--){
+            if(s[i]==' '){
+                return j-i;
+            }
+            if( s[i]!=' '&&i==0 ){
+                return j-i+1;
+            }
+        }
+        return 0;
+    }
+};
