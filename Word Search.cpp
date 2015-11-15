d6e85c460c7132934f6cbdbb117834d68333171f
Jean <j.z.feng@foxmail.com>
2015-11-16 00:02:47 +0800

    Create Word Search.cpp

---
+++ Word Search.cpp
@@ -0,0 +1,30 @@
+class Solution {
+//https://leetcode.com/problems/word-search/
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if(board.empty())   return false;
+        if(word.empty())    return false;
+        
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(existHelper(board,word,i,j,0))   return true;
+            }
+        }
+        return false;
+    }
+    
+    bool existHelper(vector<vector<char> >&board,string word,int i,int j,int k){
+        if(k==word.size())  return true;
+        
+        if(i<0||i>=board.size()||j<0||j>=board[0].size())   return false;
+        if(board[i][j]!=word[k])    return false;
+        
+        board[i][j]='#';
+        
+        bool result=(existHelper(board,word,i-1,j,k+1) || existHelper(board,word,i+1,j,k+1)
+                    ||existHelper(board,word,i,j-1,k+1) || existHelper(board,word,i,j+1,k+1));
+        board[i][j]=word[k];
+        
+        return result;
+    }
+};
