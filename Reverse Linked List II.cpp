7fecc982686d79da37556d148365e7230200dcc4
Jean <j.z.feng@foxmail.com>
2015-11-19 23:44:41 +0800

    Create Reverse Linked List II.cpp

---
+++ Reverse Linked List II.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        /*ListNode *dummy=new ListNode(0);
+        dummy->next = head;
+        ListNode *slow = dummy;
+        n -= m;
+        while (--m)
+            slow = slow->next;
+        ListNode *fast = slow->next, *tmp;
+        while (n--) {
+            tmp = fast->next;
+            fast->next = fast->next->next;
+            tmp->next = slow->next;
+            slow->next = tmp;
+        }
+        return dummy->next;*/
+        
+        ListNode *dummy=new ListNode(0);
+        dummy->next=head;
+        
+        ListNode *slow=dummy;
+        n=n-m;//这一句话不能够放在下面循环的后面，因为m变了。
+        while(--m){
+            slow=slow->next;//在m个之前的一个
+        }
+        
+        ListNode *fast=slow->next,*tmp;
+        while(n--){
+            tmp=fast->next;
+            fast->next=fast->next->next;
+            
+            tmp->next= slow->next;
+            slow->next=tmp;
+            //fast=slow->next;
+        }
+        return dummy->next;
+    }
+};
