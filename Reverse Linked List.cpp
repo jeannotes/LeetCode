145a24dd5ded4d1f925ca18629f4083a7757b296
Jean <j.z.feng@foxmail.com>
2015-11-19 23:43:55 +0800

    Create Reverse Linked List.cpp

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+//还有一种递归的写法。
+//https://github.com/JSRGFJZ/leetcode-5/blob/master/algorithms/reverseLinkedList/reverseLinkedList.cpp#L38
+    ListNode* reverseList(ListNode* head) {
+        ListNode *tail=NULL;
+        
+        if(head==NULL)  return head;
+        
+        while(head!=NULL){
+            ListNode *tmp=head->next;
+            head->next=tail;
+            tail=head;
+            head=tmp;
+        }
+        head=tail;
+        return head;
+    }
+};
