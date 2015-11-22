b721cbb9e597a137cd65523750935365734a0171
Jean <j.z.feng@foxmail.com>
2015-11-22 09:15:13 +0800

    Create Min Stack.cpp

---
+++ Min Stack.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+public:
+    stack<int>_stack;
+    stack<int>_min_stack;
+    
+    void push(int x) {
+        _stack.push(x);
+        if(x<=getMin()){
+            _min_stack.push(x);
+        }
+    }
+
+    void pop() {
+        if(!_stack.empty()){
+            if(_stack.top()==_min_stack.top()){
+                _min_stack.pop();
+            }
+            
+            _stack.pop();
+        }
+    }
+
+    int top() {
+        if(!_stack.empty()){
+            return _stack.top();
+        }
+        return 0;
+    }
+
+    int getMin() {
+        if(!_min_stack.empty()){
+            return _min_stack.top();
+        }
+        return INT_MAX;
+    }
+};
