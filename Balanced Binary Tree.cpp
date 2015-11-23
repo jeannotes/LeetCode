ebdb8ba32bd6b2b6fa6d0c31b0f190b0408160ba
Jean <j.z.feng@foxmail.com>
2015-11-23 20:00:41 +0800

    Create Balanced Binary Tree.cpp

---
+++ Balanced Binary Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL)  return true;
+        
+        int left=treeDepth(root->left);
+        int right=treeDepth(root->right);
+        
+        if(abs(left-right)>1)   return false;
+        
+        return isBalanced(root->left)&&isBalanced(root->right);
+    }
+    
+    int treeDepth(TreeNode* root){
+        if(root==NULL)  return 0;
+        
+        int left=1,right=1;
+        left+=treeDepth(root->left);
+        right+=treeDepth(root->right);
+        
+        return max(left,right);
+    }
+};
