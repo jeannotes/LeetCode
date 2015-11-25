3d39c92fa0bbf920ce62309a269eee20e214ddfb
Jean <j.z.feng@foxmail.com>
2015-11-25 22:10:52 +0800

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-//O(M + N) operation
-    
-    double min(double a, double b) {
-        return a > b ? b : a;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int len=nums1.size()+nums2.size();
+        
+        if(len%2)//奇数
+            return  findKthElement( nums1 ,nums2 , (len+1)/2 ) ;
+        else//偶数    
+            return  (findKthElement(nums1,nums2,len/2)+findKthElement( nums1 , nums2 , len/2+1 ))*0.5 ;
     }
-
-    double findKthElement(vector<int>& nums1, vector<int>& nums2,int k) {
+    
+    double min(double x,double y){
+        return x<y?x:y;
+    }
+    
+    double findKthElement(vector<int>& nums1,vector<int>& nums2,int k){
         if(nums1.size()>nums2.size()){
-            return findKthElement(nums2, nums1,k);
+            return findKthElement(nums2,nums1,k);
         }
-        //处理有一边是空的情况
+        
         if(nums1.empty())   return nums2[k-1];
         if(nums2.empty())   return nums1[k-1];
         
@@ -18,26 +25,13 @@ public:
         
         int ia=min(k/2,nums1.size());
         int ib=k-ia;
-        
-        if(nums1[ia-1]<nums2[ib-1]){//前面ia个数都是没用的
-            vector<int> v(nums1.begin()+ia,nums1.end());
-            return findKthElement(v, nums2,k-ia);//本来找第k大的，前面ia个没用，自然变成找k-ia大的。
+        if(nums1[ia-1]<nums2[ib-1]){
+            vector<int> new_nums1(nums1.begin()+ia,nums1.end());
+            return findKthElement( new_nums1 , nums2 , k-ia ) ;
+        }if(nums1[ia-1]>nums2[ib-1]){
+            vector<int> new_nums2(nums2.begin()+ib,nums2.end());
+            return findKthElement( nums1 , new_nums2 , k-ib ) ;
         }
-        if(nums1[ia-1]>nums2[ib-1]){//前面ia个数都是没用的
-            vector<int> v(nums2.begin()+ia,nums2.end());
-            return findKthElement(nums1, v,k-ia);//本来找第k大的，前面ia个没用，自然变成找k-ia大的。
-        }
-        
         return nums1[ia-1];
-    }
-    
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len=nums1.size()+nums2.size();
-        
-        if(len&0x01){//奇数个
-            return findKthElement(nums1,nums2,(len+1)/2);
-        }else{
-            return (findKthElement(nums1,nums2,len/2)+findKthElement(nums1,nums2,len/2+1))*0.5;
-        }
     }
 };
