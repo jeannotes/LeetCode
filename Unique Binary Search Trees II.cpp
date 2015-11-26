3c7291d536765f4da6f9e30f7742bd58f05a83ed
Jean <j.z.feng@foxmail.com>
2015-11-26 18:18:15 +0800

    Update Unique Binary Search Trees II.cpp

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -1,29 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution1 {
 public:
     vector<TreeNode*> generateTrees(int n) {
-        vector<vector<TreeNode *> >dp(n+1);
         if(n<1){
-            dp[0].push_back(NULL);
-            return dp[0];
+            vector<TreeNode*> dp(NULL);
+            return dp;
         }
-        
-        dp[0].push_back(NULL);dp[1].push_back(new TreeNode(1));//数字1没有其他节点
+        vector<vector<TreeNode*> >dp(n+1);
+        dp[0].push_back(NULL);
+        dp[1].push_back(new TreeNode(1));
         
         for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){//单独空的节点不考虑了，所以从1开始
+            for(int j=1;j<=i;j++){
                 for(int k=0;k<dp[j-1].size();k++){
                     for(int w=0;w<dp[i-j].size();w++){
-                        //dp[i]  push 的次数就是k*w的结果，所以每次都是push i一次
-                        //然后他的back的left指向dp[j]的
                         dp[i].push_back(new TreeNode(j));
                         dp[i].back()->left=dp[j-1][k];
                         traverse_add_copy(dp[i-j][w],dp[i].back()->right,j);
@@ -33,11 +22,9 @@ public:
         }
         return dp[n];
     }
-  
+
     void traverse_add_copy(TreeNode *&root,TreeNode *&dst,int val){
-        if(root==NULL){
-            return;
-        }
+        if(root==NULL)  return;
         dst=new TreeNode(root->val+val);
         if(root->left){
             traverse_add_copy(root->left,dst->left,val);
@@ -47,7 +34,6 @@ public:
         }
     }
 };
-
 
 class Solution2 {
 public:
