caddd84555efda97d43eea4c09d5d3fa0e7c3c0d
Jean <j.z.feng@foxmail.com>
2015-11-29 08:35:54 +0800

    Update Single Number II.cpp

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -21,17 +21,16 @@ public:
 //第二种解法：位操作法
     int singleNumber(vector<int>& nums) {
         int count[32]={0};
-        int result=0;
+        int res=0;
         for(int i=0;i<32;i++){
+            //它是一位一位的操作,当i=3的时候，count[j]<<3，右移3位，使得能够计算第三位的总出现数。当然最终判断是否是3的倍数
             for(int j=0;j<nums.size();j++){
                 if(nums[j]>>i&1){
-                    //它是一位一位的操作,当i=3的时候，count[j]<<3，右移3位，使得能够计算第三位的总出现数。当然最终判断是否是3的倍数
                     count[i]++;
                 }
             }
-            result |= (count[i]%3)<<i;//一开始卡在这里，没能够想到 如果一个数单独出现1次，那么一定是每一位都不是3的倍数，所以
-            //count[i]%3<<i才有意义
+            res |= (count[i]%3)<<i;
         }
-        return result;
+        return res;
     }
 };
