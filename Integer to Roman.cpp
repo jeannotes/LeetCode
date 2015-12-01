d3451d08803c3e6dabbfa93f7d1f3ed5911f95b1
Jean <j.z.feng@foxmail.com>
2015-12-01 23:37:23 +0800

    Update Integer to Roman.cpp

--- Integer to Roman.cpp
+++ Integer to Roman.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string intToRoman(int num) {
         string roms[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        //如何想到这些方法的，罗马数字的排列还是很有规律的。
         int nums[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string res;
         for(int i=0;i<13&&num>0;i++){
