e409068a0393d60673609ccfc4b8410b80dcb891
Jean <j.z.feng@foxmail.com>
2015-12-03 00:15:22 +0800

    Update Longest Common Prefix.cpp

--- Longest Common Prefix.cpp
+++ Longest Common Prefix.cpp
@@ -1,20 +1,14 @@
-class Solution
-{
+class Solution {
+//https://leetcode.com/problems/longest-common-prefix/
 public:
-	string longestCommonPrefix(vector<string> &strs){
-		if (strs.empty()){
-			return "";
-		}
-		else{
-			for (int i=0;i<strs[0].length();i++){
-				for (int j=1;j<strs.size();j++){
-					if (i>=strs[j].length()||
-						strs[j][i]!=strs[0][i]){
-						return strs[0].substr(0,i);
-					}
-				}
-			}
-		}
-		return strs[0];
-	}
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())    return "";
+        for(int i=0;i<strs[0].size();i++){
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]!=strs[0][i])
+                    return strs[0].substr(0,i);
+            }
+        }//本题的一个疑问就是两个不同字母“a”,"b",为何输出的是a。
+        return strs[0];
+    }
 };
