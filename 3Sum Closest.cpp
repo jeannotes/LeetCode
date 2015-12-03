7a7b59a69b2f2a64f9c9bb0f2fa1b635b57c9b57
Jean <j.z.feng@foxmail.com>
2015-12-03 09:47:45 +0800

    Update 3Sum Closest.cpp

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -1,46 +1,26 @@
 class Solution {
+//https://leetcode.com/problems/3sum-closest/
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int length=nums.size();
         sort(nums.begin(),nums.end());
-        int left=0,right=length-1;
         int res=INT_MAX,result=target;
-        for(int i=0;i<length-2;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            
-            int a=nums[i];
-            int left=i+1,right=length-1;
+        for(int i=0;i<=nums.size()-3;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int left=i+1,right=nums.size()-1;
             while(left<right){
-                int b=nums[left],c=nums[right];
-                int sum=a+b+c;
-                
-                
-                if(sum==target){
+                int sum=nums[right]+nums[left]+nums[i];
+                if(sum==target)
                     return target;
-                }else{
-                    if(abs(target-sum)<=res){
-                        res=abs(target-sum);
-                        result=sum;
+                else{
+                    if(abs(sum-target)<res){
+                        res=abs(sum-target);
+                        result = sum;
                     }
-                    
-                    if(sum>target){
+                    if(sum>target)
                         right--;
-                        while(nums[right]==nums[right+1]){
-                            right--;
-                        }
-                        
-                    }
-                    
-                    if(sum<target){
+                    if(sum<target)
                         left++;
-                        while(nums[left]==nums[left-1]){
-                            left++;
-                        }
-                    }
                 }
-                
             }
         }
         return result;
