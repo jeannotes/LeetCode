d91c9d6cb6a76e14ef9eb8ba801d4d4013f3cf2b
Jean <j.z.feng@foxmail.com>
2015-12-07 14:56:32 +0800

    Update Remove Nth Node From End of List.cpp

--- Remove Nth Node From End of List.cpp
+++ Remove Nth Node From End of List.cpp
@@ -1,40 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+//注意快慢指针法
 class Solution {
 public:
-//http://www.programcreek.com/2014/05/leetcode-remove-nth-node-from-end-of-list-java/
-//注意快慢指针法
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode *dummy=head;
-        int len=listLen(head);//head的长度
+        int len=getLength(head);
+        ListNode* dummy=head;
         if(n>len)   return NULL;
-        if(n==len) return head->next;
+        if(n==len)  return head->next;
         
+        // 1-2-3-(4)-5
         int tem=len-n;
-        while(tem>=2){
+        while(--tem){
             dummy=dummy->next;
-            tem--;
         }
-        if(n==1)    {
-            dummy->next=NULL;return head;
-        }
-        ListNode *tem2=dummy->next->next;
-        dummy->next=tem2;
+        dummy->next=dummy->next->next;
         return head;
     }
     
-    //写一个计算链表长度的函数
-    int listLen(ListNode *head){
-        if(head==NULL){
+    int getLength(ListNode* head){
+        if(head!=NULL){
+            return 1+getLength(head->next);
+        }else
             return 0;
-        }else{
-            return 1+listLen( head->next );
-        }
     }
 };
