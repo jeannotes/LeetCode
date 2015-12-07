b992aa205b0ffbdf02656c44f96aedf4e9f68f17
Jean <j.z.feng@foxmail.com>
2015-12-07 15:24:59 +0800

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -1,34 +1,27 @@
-class Solution
-{
+class Solution {
 public:
-	bool isValid(string s){
-		stack<char> stk;
-		int len=s.length();
-
-		for (int i=0;i<len;i++){
-			if (s[i]=='['||s[i]=='{'||s[i]=='('){
-				stk.push(s[i]);
-			}
-			else {
-				if (stk.empty()){
-					return false;
-				}
-				else if (stk.top()=='['&&s[i]==']'){
-					stk.pop();
-				}
-				else if (stk.top()=='{'&&s[i]=='}'){
-					stk.pop();
-				}
-				else if (stk.top()=='('&&s[i]==')'){
-					stk.pop();
-				}
-				else{
-					return false;
-				}
-			}
-		}
-		return stk.empty();
-	}
-protected:
-private:
+    bool isValid(string s) {
+        if(s.empty())
+            return false;
+        int len=s.length();
+        stack<char>stk;
+        
+        for(int i=0;i<len;i++){
+            if(s[i]=='['||s[i]=='{'||s[i]=='('){
+                stk.push(s[i]);
+            }else{
+                if(stk.empty()){
+                    return false;
+                }else if(stk.top()=='{'&&'}'==s[i]){
+                    stk.pop();
+                }else if(stk.top()=='['&&']'==s[i]){
+                    stk.pop();
+                }else if(stk.top()=='('&&')'==s[i]){
+                    stk.pop();
+                }else
+                    return false;
+            }
+        }
+        return stk.empty();//假如出现“（）”,没有这句话，就会没有数值输出
+    }
 };
