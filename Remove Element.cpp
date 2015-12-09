4932f637e97c00154a63a80081b3bcea05265fe3
Jean <j.z.feng@foxmail.com>
2015-12-09 21:59:04 +0800

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        if(nums.empty()){
+//本质上与之前的remove duplicates 是相同的思路。
+        if(nums.empty())
             return 0;
-        }
-        
-        int k=0;
+        int len=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val){
-                nums[k++]=nums[i];
-            }
+            if(nums[i]!=val)
+                nums[len++]=nums[i];
         }
-        
-        return k;
+        return len;
     }
 };
