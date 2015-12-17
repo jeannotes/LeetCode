b756e0079c5d16af075581d3e713500f9ee789a1
Jean <j.z.feng@foxmail.com>
2015-12-17 17:22:10 +0800

    Update Trapping Rain Water.cpp

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-		int n=height.size();//数组长度
-
-		if (n==0||n==1)
-		{
-			return 0;
-		}
-		else
-		{
-			int i=0;
-			vector<int> LeftMostHeight(n);//定义左右两端距离自己最高的数组
-			vector<int> RightMostHeight(n);
-
-			int lheight=0;
-			//计算左边的距离每一点的最大高度
-			for ( i = 0; i < n; i++)
-			{
-				LeftMostHeight[i]=lheight;
-				lheight=height[i]>lheight?height[i]:lheight;
-			}
-			lheight=0;
-			//计算右边的距离每一点的最大高度
-			for ( i = n-1; i >=0; i--)
-			{
-				RightMostHeight[i]=lheight;
-				lheight=lheight>height[i]?lheight:height[i];
-			}
-			//至此数组计算完毕
-
-			//接下来开始计算
-			int res=0;
-			for (int i = 0; i < n; i++)
-			{
-			    //min(LeftMostHeight[i],RightMostHeight[i])-height[i]
-				res+=(min(LeftMostHeight[i],RightMostHeight[i])-height[i])>0?(min(LeftMostHeight[i],RightMostHeight[i])-height[i]):0;
-			}
-			return res;
-		}
-		
+        if(height.empty())
+            return 0;
+        int len=height.size();
+        int leftHeight[len],rightHeight[len];
+        int leftTemp=0,rightTemp=0;
+        int res=0;
+        
+        //从左往右扫描，距离每一点最大点
+        for(int i=0;i<len;i++){
+            leftHeight[i]=leftTemp;
+            leftTemp=max(height[i],leftTemp);
+        }
+        
+        //从右往左扫描，距离每一点最大点
+        for(int i=len-1;i>=0;i--){
+            rightHeight[i]=rightTemp;
+            rightTemp=max(height[i],rightTemp);
+        }
+        
+        for(int i=0;i<len;i++){
+            int tem=(min(leftHeight[i],rightHeight[i])-height[i])>0?(min(leftHeight[i],rightHeight[i])-height[i]):0;
+            res+=tem;
+        }
+        
+        return res;
     }
 };
