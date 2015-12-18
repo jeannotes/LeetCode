53b916fc30828b07df7538cf62d2efde0fcc9242
Jean <j.z.feng@foxmail.com>
2015-12-18 23:30:45 +0800

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -2,23 +2,17 @@ class Solution {
 //https://leetcode.com/problems/jump-game/
 public:
     bool canJump(vector<int>& nums) {
-        
+        if(nums.empty())
+            return false;
         int step=nums[0];
         
-        if(nums.empty()){
-            return false;
-        }
         for(int i=1;i<nums.size();i++){
             if(step>0){
                 step--;
-                step=max(nums[i],step);
-            }else{
+                step=max(step,nums[i]);
+            }else
                 return false;
-            }
         }
-        
         return true;
-        //本题我觉得一个需要考虑的就是最后的i条件判断是i<nums.size()，而不是i<nums.size()-1；
-        //究其原因，我认为觉得可能出现[0,1]的可能性。
     }
 };
