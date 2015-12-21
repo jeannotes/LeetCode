4c9fab77fbd7f2a488f742f7b15825a31a3d5821
Jean <j.z.feng@foxmail.com>
2015-12-21 22:27:13 +0800

    Update Group Anagrams.cpp

--- Group Anagrams.cpp
+++ Group Anagrams.cpp
@@ -22,5 +22,30 @@ public:
         }
         return res;
     }
+};
 
+// 有错误程序
+class Solution2 {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>res;
+        if(strs.empty()||strs[0].empty())
+            return res;
+        map<string,int>group;
+        int count=0;
+        for(int i=0;i<strs.size();i++){
+            string tem=strs[i];
+            sort(tem.begin(),tem.end());
+            if(group.find(tem)==group.end()){
+                group[tem]=count;
+                count++;
+                res[group[tem]].push_back(strs[i]);
+            }else
+                res[group[tem]].push_back(strs[i]);
+        }
+        for(int i=0;i<res.size();i++){
+            sort(res[i].begin(),res[i].end());
+        }
+        return res;
+    }
 };
