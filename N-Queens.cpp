6deab2f818ca3f7c2c3f6787e2a755d0eafa2053
Jean <j.z.feng@foxmail.com>
2015-12-22 21:13:54 +0800

    Update N-Queens.cpp

--- N-Queens.cpp
+++ N-Queens.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 private:
 //https://leetcode.com/problems/n-queens/
     int colFlag;//代表列
@@ -65,5 +65,48 @@ public:
 
         queenHelper(n, answer, result);
         return result;
+    }
+};
+
+
+class Solution2 {
+    //http://bangbingsyb.blogspot.sg/2014/11/leetcode-n-queens-i-ii.html
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>res;
+        vector<string>tem;
+        vector<int>saveCol;
+        solveHelper(n,0,saveCol,tem,res);
+        return res;
+    }
+    
+    void solveHelper(int n,int row,vector<int>&saveCol,vector<string> &tem,vector<vector<string>>&res){
+        if(row ==n){
+            res.push_back(tem);
+            return;
+        }
+        
+        for(int col=0;col<n;col++){
+            if(isValid( saveCol,row,col)){
+                string s(n,'.');
+                saveCol.push_back(col);
+                s[col]='Q';
+                tem.push_back(s);
+                solveHelper(n,row+1,saveCol,tem,res);
+                s[col]='.';
+                saveCol.pop_back();
+                tem.pop_back();
+            }
+        }
+    }
+    
+    bool isValid(vector<int>&saveCol,int row,int col){
+        if(row<saveCol.size())
+            return false;
+        for(int i=0;i<saveCol.size();i++){
+            if(col==saveCol[i]||abs(i-row)==abs(col-saveCol[i]))
+                return false;
+        }
+        return true;
     }
 };
