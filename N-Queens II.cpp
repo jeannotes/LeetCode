2debbf447acd9b411f378c6562ba4f86ad7de9a8
Jean <j.z.feng@foxmail.com>
2015-12-22 21:18:29 +0800

    Update N-Queens II.cpp

--- N-Queens II.cpp
+++ N-Queens II.cpp
@@ -71,3 +71,43 @@ public:
         
     }
 };
+
+
+class Solution2 {
+public:
+    int count=0;
+    int totalNQueens(int n) {
+        vector<int>saveCol;
+        solveHelper( n,0,saveCol );
+        return count;
+    }
+    
+    void solveHelper(int n,int row,vector<int>&saveCol ){
+        if(row ==n){
+            count++;
+            return;
+        }
+        
+        for(int col=0;col<n;col++){
+            if(isValid( saveCol,row,col)){
+                saveCol.push_back(col);
+                solveHelper(n,row+1,saveCol );
+                saveCol.pop_back();
+            }
+        }
+    }
+    
+    bool isValid(vector<int>&saveCol,int row,int col){
+        if(row<saveCol.size())
+            return false;
+        for(int i=0;i<saveCol.size();i++){
+            if(col==saveCol[i]||abs(i-row)==abs(col-saveCol[i]))
+                return false;
+        }
+        return true;
+    }
+};
+
+ 
+    
+ 
