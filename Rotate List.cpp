e6a4d7e1b7a94240bdcd593377386a316c345d7a
Jean <j.z.feng@foxmail.com>
2015-12-24 09:01:47 +0800

    Update Rotate List.cpp

--- Rotate List.cpp
+++ Rotate List.cpp
@@ -1,39 +1,33 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-//https://leetcode.com/problems/rotate-list/
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL||head->next==NULL||k==0)
-            return head;
-        ListNode *last=head;//寻找最后节点
-        
-        int count=1;
-        while(last->next!=NULL){
-            last=last->next;
-            count++;
-        }
-        
-        k=k%count;
-        if(k==0)
+        if(head==NULL||k==0)
             return head;
             
-        ListNode *tmp=head;
+        int len=getLength(head);
+        ListNode *last=head;
+        while(last->next!=NULL)
+            last=last->next;
         
-        for(int i=1;i<count-k;i++){
-            //k是从后往前看的
+        k=k%len;
+        if(k==0)
+            return head;
+        
+        ListNode *tmp=head;
+        for(int i=1;i<len-k;i++){
             tmp=tmp->next;
         }
-        //此时tmp指着例题里面的3
+        
         last->next=head;
         head=tmp->next;
         tmp->next=NULL;
         return head;
+    }
+    
+    int getLength(ListNode *head){
+        if(head==NULL)
+            return 0;
+        else
+            return 1+getLength(head->next);
     }
 };
