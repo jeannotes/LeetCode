649debf25bf888ce48234937aed24b641e4f7bef
Jean <j.z.feng@foxmail.com>
2015-12-24 21:37:28 +0800

    Update Minimum Path Sum.cpp

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -6,7 +6,7 @@ public:
         if(m==0||n==0){
             return 0;
         }
-//        int sum[m][n];
+        
         for(int j=1;j<n;j++){
             grid[0][j]=grid[0][j-1]+grid[0][j];
         }
@@ -19,7 +19,6 @@ public:
                 grid[i][j]=min(grid[i-1][j],grid[i][j-1])+grid[i][j];
             }
         }
-        
         return grid[m-1][n-1];
     }
 };
