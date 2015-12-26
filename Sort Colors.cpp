84329c5836ceac82ff768776554b6a0c998bcdd3
Jean <j.z.feng@foxmail.com>
2015-12-27 00:59:57 +0800

    Update Sort Colors.cpp

--- Sort Colors.cpp
+++ Sort Colors.cpp
@@ -1,25 +1,19 @@
 class Solution1 {
-//https://leetcode.com/problems/sort-colors/
 public:
-	void sortColors(vector<int>& nums) {
-		int len=nums.size();
-		int one=0,two=len-1;
-
-		for (int i=0;i<=two;i++){
-			if (nums[i]==0){
-				swap(nums[i],nums[one]);
-				one++;
-			}
-			if (nums[i]==2){
-				swap(nums[i],nums[two]);
-				two--;
-				i--;
-			}
-		}
-		
-	}
+    void sortColors(vector<int>& nums) {
+        int len=nums.size();
+        int zero=0,two=len-1;
+        
+        for(int i=0;i<=two;i++){
+            if(nums[i]==0) 
+                swap(nums[i],nums[zero++]);
+            if(nums[i]==2){
+                swap(nums[i],nums[two]);
+                two--;i--;//i的值已经变换，需-1，继续判断
+            }
+        }
+    }
 };
-
 
 class Solution2 {
 public:
