a4fffdd0e7f7d2359fb583273f30ef59d8292d54
Jean <j.z.feng@foxmail.com>
2015-12-26 19:27:31 +0800

    Update Simplify Path.cpp

--- Simplify Path.cpp
+++ Simplify Path.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int len=path.length();
-        int i=0;//代表具体位置
+        vector<string>result;
+        int len=path.length(),i=0;
         
-        vector<string> result;
         while(i<len){
-            //去掉所有的////
-            while( path[i]=='/') i++;       //  这个地方不加“ i< len”是没事的，因为开头就已经判断
-            if(i==len)    break;
+            while(i<len&&path[i]=='/'){
+                i++;
+            }
+            if(i==len)
+                break;
             
-            //找出接下来的字符
-            string temp;
-            while(i<len && path[i]!='/'){   //  这个地方不加“ i< len”不行，因为上面i++可能会导致溢出
-                temp.push_back(path[i++]);
+            string tmp;
+            while(i<len&&path[i]!='/'){
+                tmp+=path[i++];
             }
-            if(temp==".")   continue;
-            if(temp=="..") {
-                if(!result.empty()){
+            if(tmp==".")
+                continue;
+            else if(tmp==".."){
+                if(!result.empty())
                     result.pop_back();
-                }
             }
-            else {
-                result.push_back(temp);
-            }
+            else
+                result.push_back(tmp);
         }
         
-        //程序往下执行说明有数据
-        string final;
-        
-        if(result.empty()){
+        string res;
+        if(result.empty())
             return "/";
-        }
         for(int i=0;i<result.size();i++){
-            final.append("/");
-            final.append(result[i]);
+            res+='/';
+            res+=result[i];
         }
-        return final;
-    }
+        
+        return res;
+    } 
 };
