6e7347cbc0c5ea27ac8869457ab3ab3b90153378
Jean <j.z.feng@foxmail.com>
2015-12-28 21:33:59 +0800

    Update Remove Duplicates from Sorted Array II.cpp

--- Remove Duplicates from Sorted Array II.cpp
+++ Remove Duplicates from Sorted Array II.cpp
@@ -1,41 +1,24 @@
 class Solution {
-//Remove Duplicates from Sorted Array II
-//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 public:
-	int removeDuplicates(vector<int>& nums){
-		int n=nums.size();
-		if(n<2)
-			return n;
-		else
-		{
-			int len=0;
-			int j=0;
-			for (int i = 1; i < n; i++)
-			{
-				if (nums[i]!=nums[len])
-				{
-					len++;
-					nums[len]=nums[i];
-					j=0;
-				}
-				else
-				{
-					if (j==1)
-					{
-						//j=1表明已经有二次了						
-						continue;
-					}
-					else
-					{
-						j=1;
-						len++;
-						nums[len]=nums[i];
-					}
-				}
-			}
-			return len+1;
-		}		
-	}
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+        if(nums.size()==1)
+            return 1;
+        sort(nums.begin(),nums.end());
+        int len=0,count=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[len]){
+                count=1;
+                len++;nums[len]=nums[i];
+            }else{
+                if(count<2){
+                    len++;
+                    nums[len]=nums[i];
+                    count++;
+                }
+            }
+        }
+        return len+1;
+    }
 };
-//本题目一开始卡在出现[1 1 1 1 ]的情况，后来[1 1 2 2]又没有考虑到，这才在第一个if循环中添加j=0这个条件。
-//最主要就是j=0这个条件放错位置
