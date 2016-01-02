e3aebca915fa4e6bce58bede48382dd7fe50922a
Jean <j.z.feng@foxmail.com>
2016-01-02 12:35:43 +0800

    Update Unique Binary Search Trees.cpp

--- Unique Binary Search Trees.cpp
+++ Unique Binary Search Trees.cpp
@@ -3,6 +3,7 @@ public:
     int numTrees(int n) {
         //https://leetcode.com/problems/unique-binary-search-trees/
         // f(n)=f(0)*f(n-1)+f(1)*f(n-2)+f(2)*f(n-3)+...+f(n-1)*f(0)
+        //f(n)--代表左半边有几个数
         //f(1)=f(0)*f(0)   f(2)=f(0)*f(1)+f(1)*f(0)
         //f(3)=f(0)*f(2)+f(1)*f(2)+f(2)*f(0)
         vector<int> sum(n+1,0);
@@ -12,7 +13,6 @@ public:
                 sum[i]+=sum[j]*sum[i-1-j];
             }
         }
-        
         return sum[n];
     }
 };
