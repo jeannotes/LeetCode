4cebe6b13c5fb8dae98fc799e61a42fbe68bf23f
Jean <j.z.feng@foxmail.com>
2016-01-10 13:35:47 +0800

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,22 +1,18 @@
 class Solution {
-//Remove Duplicates from Sorted Array
-//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 public:
-	int removeDuplicates(vector<int>& nums){
-		int n=nums.size();
-		if(n<2)
-			return n;
-		else{
-			int len=0;
-			for(int i=1;i<n;i++){
-				if(nums[i]!=nums[len]){
-					len++;
-					nums[len]=nums[i];
-				}
-			}
-			return len+1;
-		}  
-	}
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<=0)
+            return 0;
+        int len=0;
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[len]){
+                len++;
+                nums[len]=nums[i];
+            }
+        }
+        return len+1;
+    }
 };
 		//一方面做其实位置，一方面做标记，有几个数字
 		//程序厉害的地方在于，设置了和这一区间段相同最后一位的后一位数字不同索引值。
