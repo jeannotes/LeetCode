318865058b8813417303de488ea81ec5ba8d7410
Jean <j.z.feng@foxmail.com>
2016-01-10 21:16:11 +0800

    Update Minimum Window Substring.cpp

--- Minimum Window Substring.cpp
+++ Minimum Window Substring.cpp
@@ -1,41 +1,38 @@
 class Solution {
     //http://www.cnblogs.com/TenosDoIt/p/3461301.html
+    //https://leetcode.com/discuss/72701/here-10-line-template-that-can-solve-most-substring-problems
 public:
-    string minWindow(string s, string t) {
-        int lens=s.length(),lent=t.length();
-        int number_in_t[256]={0};
-        int number_found_in_t[256]={0};
-        
-        //初始化
-        for(int i=0;i<lent;i++){
-            number_in_t[t[i]]++;
+    string minWindow(string S, string T) {
+        string result;
+        if(S.empty() || T.empty()){
+            return result;
         }
-        
-        int hasFound=0;
-        int winStart=-1,winEnd=lens;
-        
-        //start 和i 一起扫描
-        for(int i=0,start=0;i<lens;i++){
-            if(number_in_t[s[i]]!=0){
-                //s中的字符在t中存在
-                number_found_in_t[s[i]]++;
-                if(number_found_in_t[s[i]]<=number_in_t[s[i]])  hasFound++;
-                //这句话是确保当前的字符不超过t字符串里面字符的个数
-                if(hasFound==lent){//找到一个窗口
-                    while(number_in_t[s[start]]==0 || number_found_in_t[s[start]]>number_in_t[s[start]]){
-                        if(number_in_t[s[start]]!=0)    number_found_in_t[s[start]]--;//如果不剪掉，那么不能准确判断出现多少次
-                        start++;
-                    }
-                    if(winEnd-winStart>i-start){//考虑一下这边+1 还是 -1 的情况
-                        winEnd=i;winStart=start;
-                    }
-                    //这个时候以start开头的字符仅仅出现1次，减掉就可以满足下一个条件
-                    
-                    number_found_in_t[s[start]]--;//这是为了让刚刚第一个只出现一次的字符变成0，相应的hasFound也要减去1，其余不变
-                    start++;hasFound--;
-                }/**/
+        unordered_map<char, int> map;
+        unordered_map<char, int> window;
+        for(int i = 0; i < T.length(); i++){
+            map[T[i]]++;
+        }
+        int minLength = INT_MAX;
+        int letterCounter = 0;
+        for(int slow = 0, fast = 0; fast < S.length(); fast++){
+            char c = S[fast];
+            if(map.find(c) != map.end()){
+                window[c]++;
+                if(window[c] <= map[c]){
+                    letterCounter++;
+                }
+            }
+            if(letterCounter >= T.length()){
+                while(map.find(S[slow]) == map.end() || window[S[slow]] > map[S[slow]]){
+                    window[S[slow]]--;
+                    slow++;
+                }
+                if(fast - slow + 1 < minLength){
+                    minLength = fast - slow + 1;
+                    result = S.substr(slow, minLength);
+                }
             }
         }
-        return winStart!=-1? s.substr(winStart,winEnd-winStart+1):"";
+        return result;
     }
 };
