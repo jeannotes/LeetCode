014f3673b01f37fc7e6b2559b5b956813858c8fa
Jean <j.z.feng@foxmail.com>
2016-01-10 13:12:12 +0800

    Update Search in Rotated Sorted Array II.cpp

--- Search in Rotated Sorted Array II.cpp
+++ Search in Rotated Sorted Array II.cpp
@@ -61,7 +61,6 @@ public:
     bool search(vector<int>& nums, int target) {
         if(nums.empty())
             return -1;
-            sort(nums.begin(),nums.end());
         int left=0,right=nums.size()-1;
         while(left<=right){
             int mid=(left+right)/2;
