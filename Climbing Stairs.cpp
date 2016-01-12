e0cd7fb46da40e1d0166bfdf55fe1269a8b1dd59
Jean <j.z.feng@foxmail.com>
2016-01-12 08:22:08 +0800

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -3,11 +3,11 @@ public:
 //斐波那契数列，下一个数，要么之前一位的，要么之前两位的。
     int climbStairs(int n) {
         int sum[n+1];
-        sum[0]=1;
+        sum[0]=1;//往往初始化都是为true或者1;
         sum[1]=1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=n;i++)
             sum[i]=sum[i-1]+sum[i-2];
-        }
+        
         return sum[n];
     }
 };
