ed7d2d3115b2d4a4beaeaa38bb168451f93084d5
Jean <j.z.feng@foxmail.com>
2016-01-13 21:28:04 +0800

    Update Add Binary.cpp

--- Add Binary.cpp
+++ Add Binary.cpp
@@ -3,16 +3,15 @@ public:
     string addBinary(string a, string b) {
         if(a.length()<b.length())
             return addBinary(b,a);
-        b.insert(b.begin(),a.length()-b.length(),'0');//此函数在p0处插入n个字符c
+        b.insert(b.begin(),a.length()-b.length(),'0');
         int flag=0;
         for(int i=a.length()-1;i>=0;i--){
-            int tem=int(a[i]-'0')+int(b[i]-'0')+flag;
-            flag=tem/2;
-            tem%=2;
-            a[i]=char('0'+tem);
+            int sum=(int)(a[i]-'0')+(int)(b[i]-'0')+flag;
+            a[i]=char(sum%2+'0');
+            flag=sum/2;
         }
         if(flag)
-            a.insert(a.begin(),char('0'+flag));
+            a.insert(a.begin(),'1');
         return a;
     }
 };
