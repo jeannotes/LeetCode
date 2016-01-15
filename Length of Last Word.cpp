b5f332d2b2f7b9fda81b46fb987ef6b48bc63818
Jean <j.z.feng@foxmail.com>
2016-01-15 22:47:36 +0800

    Update Length of Last Word.cpp

--- Length of Last Word.cpp
+++ Length of Last Word.cpp
@@ -2,18 +2,17 @@ class Solution {
 //https://leetcode.com/problems/length-of-last-word/
 public:
     int lengthOfLastWord(string s) {
-        if(s.empty())   return 0;
-        //从后往前找第一个非" "的字符，如果没有，返回0
-        int len=s.length();
-        int j=len-1;
-        while(j>=0&&s[j]==' ')   j--;
-        if(j<0) return 0;
-        for(int i=j;i>=0;i--){
-            if(s[i]==' ')
-                return j-i;
-            if( s[i]!=' '&&i==0 )
-                return j-i+1;
+        if(s.empty())
+            return 0;
+        int i=s.length()-1,count=0;
+        while(i>=0&&s[i]==' ')
+            i--;
+        for(;i>=0;i--){
+            if(s[i]!=' ')
+                count++;
+            else
+                break;
         }
-        return 0;
+        return count;
     }
 };
