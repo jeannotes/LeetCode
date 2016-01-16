858045184283c5200a604835b56cadc26daecd93
Jean <j.z.feng@foxmail.com>
2016-01-16 10:35:44 +0800

    Update Merge Intervals.cpp

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-
-    class Compare {
-    public:
-        bool operator() (const Interval &i1, const Interval i2) {
-            return i1.start < i2.start;
-        }
+    class Compare{
+        public:
+            bool operator()(const Interval &i1,const Interval&i2){
+                return i1.start<i2.start;
+            }
     };
     
     vector<Interval> merge(vector<Interval>& intervals) {
-        vector<Interval> result;
-        if(intervals.empty()){
-            return result;
-        }
+        vector<Interval>res;
+        if(intervals.empty())
+            return res;
         Compare cmp;
-        sort(intervals.begin(),intervals.end(),cmp);//按照开头数字升序排列
-        //注意考虑两个区间合并后，他的end比下一个的start还要大
-        int i=0;
-        int k=0;
-        result.push_back(intervals[0]);//记住加入第一个数据。
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i].start>result.back().end){
-                result.push_back(intervals[i]);
-            }else{
-                //此时注意两种情况，但是总之是有交集的
-                result.back().end=max(  result.back().end,intervals[i].end  );
-            }
+        sort(intervals.begin(),intervals.end(),cmp);
+        res.push_back(intervals[0]);
+        
+        for(int i=1;i<=intervals.size()-1;i++){
+            if(intervals[i].start<=res.back().end)
+                res.back().end=max(intervals[i].end,res.back().end);
+            else
+                res.push_back(intervals[i]);
         }
-        return result;
+        return res;
     }
 };
