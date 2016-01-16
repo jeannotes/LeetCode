baf56f2aa1c69e0caa301c70fb15109c6ba42bfb
Jean <j.z.feng@foxmail.com>
2016-01-16 10:06:40 +0800

    Update Spiral Matrix.cpp

--- Spiral Matrix.cpp
+++ Spiral Matrix.cpp
@@ -13,11 +13,11 @@ public:
                 res.push_back(matrix[up][i]);
             for(int i=up;i<=down;i++)
                 res.push_back(matrix[i][right]);
-            
             if(up==down||right==left)
                 break;
             //这里不能和之前旋转矩阵一样，因为如果出现up=down，需要退出，
             //所以必须i<=down
+            //didn't understand
             for(int i=right-1;i>=left;i--)
                 res.push_back(matrix[down][i]);
             for(int i=down-1;i>up;i--)
