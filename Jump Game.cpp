c10b6c388bd1e44b0e7612783d4fc443973575e9
Jean <j.z.feng@foxmail.com>
2016-01-16 10:53:13 +0800

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -1,17 +1,13 @@
 class Solution {
-//https://leetcode.com/problems/jump-game/
 public:
     bool canJump(vector<int>& nums) {
-        if(nums.empty())
-            return false;
         int step=nums[0];
-        
         for(int i=1;i<nums.size();i++){
             if(step>0){
                 step--;
                 step=max(step,nums[i]);
             }else
-                return false;
+                return false;//step<0 , which mean it cannot jump to the iTH step;
         }
         return true;
     }
