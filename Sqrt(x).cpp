8d30697dd2692eafe79b238eadda59103f74c271
Jean <j.z.feng@foxmail.com>
2016-01-17 15:00:44 +0800

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -1,32 +1,4 @@
-class Solution1 {
-public:
-    int mySqrt(int x) {
-        if(x<=0)
-            return 0;
-        if(1<=x&&x<4)
-            return 1;
-        //不能超过 2147395599，他的根号值是46340，
-        //超过了，结果也是 46340 ，
-        //所以，最大结果是 46340 ，那么 s<=x/2 x最大也就是46340*2
-        if(x>=2*46340)
-            return binaryInt(x,1,46340);
-        return binaryInt(x,1,x/2);
-    }
-    
-    int binaryInt(int x,int left,int right){
-        if(left>right)
-            return right;
-        int mid=(left+right)/2;
-        if(mid*mid==x)
-            return mid;
-        else if(mid*mid<x)
-            return binaryInt(x,mid+1,right);
-        else
-            return binaryInt(x,left,mid-1);
-    }
-};
-
-class Solution2 {
+class Solution {
     //https://leetcode.com/discuss/24942/a-binary-search-solution
 public:
     int mySqrt(int x) {
