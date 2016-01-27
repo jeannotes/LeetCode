c566dcd7d7bb15b509ad6ea4cf73ff881a69aff7
Jean <j.z.feng@foxmail.com>
2016-01-27 11:51:58 +0800

    Update Pow(x, n).cpp

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -1,21 +1,15 @@
 class Solution {
-//https://leetcode.com/problems/powx-n/
 public:
     double myPow(double x, int n) {
-        if(n==0) return 1;//注意很多细节
-        if(n==1) return x;
-        if(x==0||x==1) return x;
-        
+        if(n==0)
+            return 1;
+        if(x==0||x==1)
+            return x;
         if(n<0){
-            if (n == INT_MIN) {
+            if(n==INT_MIN)
                 return 1;
-            }
             return myPow(1.0/x,-n);
         }
-        double half = myPow(x, n/2);
-        if(n%2==0){
-            return half * half;
-        }
-        return x*half * half;
+        return n%2==0? myPow(x*x,n/2):x*myPow(x*x,n/2);   
     }
 };
