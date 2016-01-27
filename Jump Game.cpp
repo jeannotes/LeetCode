ee7505851b1bd60bf8fc2e0760c85ee279626642
Jean <j.z.feng@foxmail.com>
2016-01-27 15:42:37 +0800

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -4,10 +4,9 @@ public:
         int step=nums[0];
         for(int i=1;i<nums.size();i++){
             if(step>0){
-                step--;
-                step=max(step,nums[i]);
+                step=max(--step,nums[i]);
             }else
-                return false;//step<0 , which mean it cannot jump to the iTH step;
+                return false;
         }
         return true;
     }
