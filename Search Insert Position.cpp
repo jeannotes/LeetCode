d2fc6958b837a9f9a0a9b8587c8e3efa29d2128e
Jean <j.z.feng@foxmail.com>
2016-01-31 12:10:59 +0800

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -1,63 +1,24 @@
-class Solution1 {
+class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int length=nums.size();
-        int i=0,j=length-1;
-        
-        if(nums[0]>target){
-            return 0;
-        }
-        
-        if(nums[length-1]<target){
-            return length;
-        }
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]<target){
-                i=mid+1;
-            }
-            if(nums[mid]>target){
-                j=mid-1;
-            }
-        }
-        
-        if(i>j){
-            return i;
-        }
-    }
-};
-
-class Solution2 {
-public:
-    int pos=0;
     int searchInsert(vector<int>& nums, int target) {
         if(nums.empty())
             return 0;
-        int find=binarySearch(0,nums.size()-1,nums,target);
-        
-        if(find!=-1)
-            return find;
-        else
-            return  pos; //1 2 4
-    }
-    
-    int binarySearch(int left,int right,vector<int>&nums,int target){
-        if (left>right){
-            pos=(right+left+2)/2;
-            //pos= left ;
-            return -1;
+        int len=nums.size();
+        if(target<nums[0])
+            return 0;
+        if(target>nums[len-1])
+            return len;
+        //这个时候保证一定在里面
+        int i=0,j=len-1,mid=0;
+        while(i<=j){
+            mid=(i+j)/2;
+            if(nums[mid]==target)
+                return mid;
+            if(nums[mid]>target)
+                j=mid-1;
+            if(nums[mid]<target)
+                i=mid+1;
         }
-            
-        int mid=(left+right)/2;
-        
-        if(nums[mid]==target)
-            return mid;
-        if(nums[mid]>target)
-            return binarySearch(left,mid-1,nums,target);
-        if(nums[mid]<target)
-            return binarySearch(mid+1,right,nums,target);
+        return i;//1 3 5 7
     }
 };
