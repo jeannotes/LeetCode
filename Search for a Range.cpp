596c2b394916c0baf9fff727928949d745631822
Jean <j.z.feng@foxmail.com>
2016-01-31 14:31:37 +0800

    Update Search for a Range.cpp

--- Search for a Range.cpp
+++ Search for a Range.cpp
@@ -1,43 +1,36 @@
 class Solution1 {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result(2,-1);
-        
-        int low=binarySearchLow(nums,target,0,nums.size()-1);
-        int high=binarySearchHigh(nums,target,0,nums.size()-1);
-        
-        if(low<=high){
-            result[0]=low;
-            result[1]=high;
-            return result;
+        vector<int>res(2,-1);
+        if(nums.empty()||target<nums[0]||target>nums[nums.size()-1])
+            return res;
+        int i=binaryLow( nums, target, 0 , nums.size()-1);
+        int j=binaryHigh( nums, target, 0 , nums.size()-1);
+        if(i<=j){//仔细想想，第一个是找小于等于的，第二个是找大于等于的
+            res[0]=i;
+            res[1]=j;
         }
-        return result;
+        return res;
     }
     
-    int binarySearchLow(vector<int> &nums,int target,int left,int right){
-        if(left>right){//关键的判断
-            return left;
-        }
-        int mid=(left+right)/2;
-        if(nums[mid]<target){//这个地方不能有等于号，有等于号，而且如果只有1个mid，就再也回不来
-            return binarySearchLow(nums,target,mid+1,right);
-        }else{
-            return binarySearchLow(nums,target,left,mid-1);
-        }
-        //写这个函数的时候思路是这样的：我先将if的两个情况写下来，再对应题目可能情况修改。
+    int binaryLow(vector<int>& nums,int target,int i,int j){
+        if(i>j)
+            return i;
+        int mid=(i+j)/2;
+        if(nums[mid]<target)
+            return binaryLow(nums,target,mid+1,j);
+        else
+            return binaryLow(nums,target,i,mid-1);
     }
     
-    int binarySearchHigh(vector<int> &nums,int target,int left,int right){
-        if(left>right){
-            return right;
-        }
-        int mid=(left+right)/2;
-        if(nums[mid]>target){//这个地方不能有等于号，有等于号，而且如果只有1个mid，就再也回不来
-            return binarySearchHigh(nums,target,left,mid-1);
-        }else{
-            return binarySearchHigh(nums,target,mid+1,right);
-        }
-        //写这个函数的时候思路是这样的：我先将if的两个情况写下来，再对应题目可能情况修改。
+    int binaryHigh(vector<int>& nums,int target,int i,int j){
+        if(i>j)
+            return j;
+        int mid=(i+j)/2;
+        if(nums[mid]>target)
+            return binaryHigh(nums,target,i,j-1);
+        else
+            return binaryHigh(nums,target,i+1,j);
     }
 };
 
