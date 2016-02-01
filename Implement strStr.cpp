37bbe5808aea2e2736d6ffd065c0c5466ae47ba5
Jean <j.z.feng@foxmail.com>
2016-02-02 00:18:03 +0800

    Update Implement strStr.cpp

--- Implement strStr.cpp
+++ Implement strStr.cpp
@@ -1,19 +1,17 @@
 class Solution {
-//代码越来越简洁了
 public:
     int strStr(string haystack, string needle) {
         if(needle.length()<1)
             return 0;
-        int length1=haystack.length(),length2=needle.length();
-        
-        for(int i=0;i<=length1-length2;i++){
+        int len1=haystack.length(),len2=needle.length();
+        for(int i=0;i<=len1-len2;i++){
             if(haystack[i]==needle[0]){
                 int j=0;
-                for(;j<length2;j++){
+                for(;j<len2;j++){
                     if(needle[j]!=haystack[i+j])
                         break;
                 }
-                if(j==length2)
+                if(j==len2)
                     return i;
             }
         }
