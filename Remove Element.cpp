63dfceb80db002fec9253922d37b79593b267dd3
Jean <j.z.feng@foxmail.com>
2016-02-02 13:54:19 +0800

    Update Remove Element.cpp

--- Remove Element.cpp
+++ Remove Element.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-//本质上与之前的remove duplicates 是相同的思路。
         if(nums.empty())
             return 0;
         int len=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val)
-                nums[len++]=nums[i];
+                nums[len++]=nums[i];//1 2 3 4 
         }
         return len;
+        //为什么是len而不是len+1，
+        //在nums数组中，只要不相同，就会从一开始重新赋值,而之后len会+1，所以不需要最后len+1
     }
 };
