a424e1644a60faa3a19af853e6fa89de4380dffa
Jean <j.z.feng@foxmail.com>
2016-02-02 14:16:10 +0800

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()<=0)
+        if(nums.empty())
             return 0;
         int len=0;
-        
         for(int i=1;i<nums.size();i++){
             if(nums[i]!=nums[len]){
-                len++;
-                nums[len]=nums[i];
+                nums[++len]=nums[i];
             }
         }
         return len+1;
+        //对比remove element，这里可以这样思考
+        //如果全部为1，一开始len是0，最后仅仅保留一个自然要+1输出
     }
 };
-		//一方面做其实位置，一方面做标记，有几个数字
-		//程序厉害的地方在于，设置了和这一区间段相同最后一位的后一位数字不同索引值。
-		//并且直接保存到数组里面，方便比较。
