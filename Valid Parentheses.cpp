03caa7e5e7080c6fe908bf824a286cc83482344f
Jean <j.z.feng@foxmail.com>
2016-02-03 12:05:03 +0800

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -25,3 +25,30 @@ public:
         return stk.empty();//假如出现“（）”,没有这句话，就会没有数值输出
     }
 };
+
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.empty())
+            return true;
+        stack<char>res;
+        if(s.length()%2!=0)
+            return false;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+                res.push(s[i]);
+            else{
+                if(res.empty())
+                    return false;
+                else{
+                    char tem=res.top();res.pop();
+                    if(tem=='('&&s[i]==')'||tem=='['&&s[i]==']'||tem=='{'&&s[i]=='}')
+                        continue;
+                    else
+                        return false;
+                }
+            }
+        }
+        return res.empty();//防止出现 “((”的情况
+    }
+};
