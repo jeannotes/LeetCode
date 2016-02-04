24fa54e7c694add659ff446263b59b6c073b19c8
Jean <j.z.feng@foxmail.com>
2016-02-04 20:59:18 +0800

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -2,34 +2,6 @@ class Solution {
 public:
     bool isValid(string s) {
         if(s.empty())
-            return false;
-        int len=s.length();
-        stack<char>stk;
-        
-        for(int i=0;i<len;i++){
-            if(s[i]=='['||s[i]=='{'||s[i]=='('){
-                stk.push(s[i]);
-            }else{
-                if(stk.empty()){
-                    return false;
-                }else if(stk.top()=='{'&&'}'==s[i]){
-                    stk.pop();
-                }else if(stk.top()=='['&&']'==s[i]){
-                    stk.pop();
-                }else if(stk.top()=='('&&')'==s[i]){
-                    stk.pop();
-                }else
-                    return false;
-            }
-        }
-        return stk.empty();//假如出现“（）”,没有这句话，就会没有数值输出
-    }
-};
-
-class Solution {
-public:
-    bool isValid(string s) {
-        if(s.empty())
             return true;
         stack<char>res;
         if(s.length()%2!=0)
