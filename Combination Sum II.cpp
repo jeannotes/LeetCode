f70fb784ea58f6a1b7a26d9c14a3d066b9734231
Jean <j.z.feng@foxmail.com>
2016-02-06 17:59:10 +0800

    Update Combination Sum II.cpp

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -25,3 +25,32 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>>res;
+    vector<int>tem;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        sort(candidates.begin(),candidates.end());
+        combinationHelper(candidates,0,target);
+        return res;
+    }
+    
+    void combinationHelper(vector<int>& candidates,int start,int target){
+        if(target==0){
+            res.push_back(tem);
+            return;
+        }
+        if(target<0)
+            return;
+        for(int i=start;i<candidates.size();i++){
+            if(i>start&&candidates[i]==candidates[i-1])
+                continue;
+            tem.push_back(candidates[i]);
+            combinationHelper(candidates,i+1,target-candidates[i]);
+            tem.pop_back();
+        }
+    }
+};
+//第二种方法也很容易理解，1 1 1,再加一个和之前相同的数字，抛弃
