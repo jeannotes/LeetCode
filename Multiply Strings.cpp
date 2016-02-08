468e3e2da0900f6821afe7619101cdc8e237d26f
Jean <j.z.feng@foxmail.com>
2016-02-08 22:42:41 +0800

    Update Multiply Strings.cpp

--- Multiply Strings.cpp
+++ Multiply Strings.cpp
@@ -4,56 +4,55 @@ public:
         if(num1.length()<num2.length()){
             return multiply( num2, num1);
         }
+        vector<string>res;
+        int len1=num1.length(),len2=num2.length();
         
-        int m=num1.length(),n=num2.length();
-        string res;
-        for(int i=n-1;i>=0;i--){
-            string tem=multiplySingleNumber( num1,num2[i]);
-            string tem2(n-1-i,'0');
-            tem+=tem2;
-            res=addString(res,tem);
+        for(int i=len2-1;i>=0;i--){
+            string tem=string_multiply(num1,num2[i]);
+            tem.append(len2-i-1,'0');
+            res.push_back(tem);
         }
-        
-        return res;
+        for(int i=1;i<res.size();i++){
+            res[0]=string_add(res[0],res[i]);
+        }
+        return res[0];
     }
     
-    string multiplySingleNumber(string num,char c){
+    string string_multiply(string num,char c){
         if(c=='0')
             return "0";
         int flag=0;
+        string res;
         for(int i=num.length()-1;i>=0;i--){
-            int tem= (num[i]-'0')*(c-'0')+flag;
+            int tem=int(num[i]-'0') * int(c-'0')+flag;
+            res+=char(tem%10+'0');
             flag=tem/10;
-            num[i]=(tem%10)+'0';
         }
-        if(flag>0){
-            num.insert(num.begin(),'0'+flag);
-        }
-        
-        return num;
+        if(flag)
+            res+=char(flag+'0');
+        reverse(res.begin(),res.end());
+        return res;
     }
     
-    string addString(string num1,string num2){
+    string string_add(string num1,string num2){
         if(num1.length()<num2.length())
-            return addString(num2,num1);
+            return string_add(num2,num1);
         int len1=num1.length(),len2=num2.length();
-        //len1>len2
-        len1--;len2--;
-        int flag=0;
-        while(len1>=0&&len2>=0){
-            int tem=int(num1[len1]-'0')+int(num2[len2]-'0')+flag;
-            flag=tem/10;tem=tem%10;
-            num1[len1]=char('0'+tem%10);
-            len1--;len2--;
+        int i=len1-1,j=len2-1,flag=0;
+        string res;
+        while(i>=0&&j>=0){
+            int tem=(int)(num1[i]-'0')+(int)(num2[j]-'0')+flag;
+            num1[i]=char(tem%10+'0');
+            flag=tem/10;i--;j--;
         }
-        while(len1>=0){
-            int tem=int(num1[len1]-'0')+flag;
-            flag=tem/10;
-            num1[len1]=char('0'+tem%10);
-            len1--;
+        while(i>=0){
+            int tem=(int)(num1[i]-'0')+flag;
+            num1[i]=char(tem%10+'0');
+            flag=tem/10;i--;
         }
-        if(flag!=0)
-            num1.insert(num1.begin(),char('0'+flag));
+        if(flag)
+            num1.insert(num1.begin(),char(flag+'0'));
         return num1;
     }
 };
+//bug-exist  都是小错误，但是继续加油！
