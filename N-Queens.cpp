11d86e3cd6f455271de14efd59a106238361fa9a
Jean <j.z.feng@foxmail.com>
2016-02-10 23:26:53 +0800

    Update N-Queens.cpp

--- N-Queens.cpp
+++ N-Queens.cpp
@@ -1,39 +1,35 @@
 class Solution {
-    //http://bangbingsyb.blogspot.sg/2014/11/leetcode-n-queens-i-ii.html
-public:
+public://http://bangbingsyb.blogspot.sg/2014/11/leetcode-n-queens-i-ii.html
+    vector<int> saveCol;// 存储每一行中的列数字
+    vector<vector<string>> res;
+    vector<string> tem;
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>res;
-        vector<string>tem;
-        vector<int>saveCol;// 存储每一行中的列数字
-        solveHelper(n,0,saveCol,tem,res);
+        solveHelper(n,0);
         return res;
     }
     
-    void solveHelper(int n,int row,vector<int>&saveCol,vector<string> &tem,vector<vector<string>>&res){
-        if(row ==n){
-            res.push_back(tem);
-            return;
+    void solveHelper(int n,int row){
+        if(row==n){
+            res.push_back(tem);return;
         }
-        
         for(int col=0;col<n;col++){
-            if(isValid( saveCol,row,col)){
+            if(isValid(row,col)){
                 string s(n,'.');
-                saveCol.push_back(col);
                 s[col]='Q';
                 tem.push_back(s);
-                solveHelper(n,row+1,saveCol,tem,res);
-                s[col]='.';
+                saveCol.push_back(col);
+                solveHelper(n,row+1);
                 saveCol.pop_back();
                 tem.pop_back();
+                s[col]='.';
             }
         }
     }
-    
-    bool isValid(vector<int>&saveCol,int row,int col){
+    bool isValid(int row,int col){
         if(row<saveCol.size())
             return false;
         for(int i=0;i<saveCol.size();i++){
-            if(col==saveCol[i]||abs(i-row)==abs(col-saveCol[i]))
+            if(saveCol[i]==col||abs(saveCol[i]-col)==abs(row-i))
                 return false;
         }
         return true;
