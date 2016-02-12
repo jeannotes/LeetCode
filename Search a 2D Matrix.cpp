39890b83e196e0898ab6664d90d30e11cb577668
Jean <j.z.feng@foxmail.com>
2016-02-12 12:03:43 +0800

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -1,22 +1,19 @@
 class Solution {
-    //https://leetcode.com/problems/search-a-2d-matrix/
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.empty()||matrix[0].empty())
-            return false;
         int m=matrix.size(),n=matrix[0].size();
         int i=0,j=m*n-1;
-        
         while(i<=j){
             int mid=(i+j)/2;
             int row=(mid/n),col=mid%n;
             if(matrix[row][col]==target)
                 return true;
-            if(matrix[row][col]>target)
-                j--;
             if(matrix[row][col]<target)
-                i++;
+                i=mid+1;
+            if(matrix[row][col]>target)
+                j=mid-1;
         }
         return false;
     }
 };
+//  int row=(mid/n),col=mid%n;  在这个地方出错
