f75d118521aa7bb64315819d64b1b0a2e94712a2
Jean <j.z.feng@foxmail.com>
2016-02-12 09:58:50 +0800

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-//https://leetcode.com/problems/unique-paths/
     int uniquePaths(int m, int n) {
-        if(m==0||n==0){
-            return 0;
+        int dp[m][n];
+        dp[0][0]=1;
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0];
         }
-        int sum[m][n];
-        for(int i=0;i<m;i++){
-            sum[i][0]=1;//一直往下走只有一条路
-        }
-        for(int i=0;i<n;i++){
-            sum[0][i]=1;//一直往右走只有一条路
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1];
         }
         
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                sum[i][j]=sum[i-1][j]+sum[i][j-1];
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
         }
-        return sum[m-1][n-1];
+        return dp[m-1][n-1];
     }
 };
+//  还是没有做到bug-free
