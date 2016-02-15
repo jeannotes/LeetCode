f2a38b6582390726024b425fe7396c87408ec295
Jean <j.z.feng@foxmail.com>
2016-02-15 22:57:57 +0800

    Update Gray Code.cpp

--- Gray Code.cpp
+++ Gray Code.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> res;
-        if(n==0){
-            res.push_back(0);
-            return res;
-        }
-        res.push_back(0);
-        res.push_back(1);
-        if(n==1)    
-            return res;
-        
-        for(int i=2;i<=n;i++){
-            int high=pow(2,i-1);
-            vector<int> tmp;
-            for(int j=res.size()-1;j>=0;j--){
-                tmp.push_back(high+res[j]);
-            }
-            for(int j=0;j<tmp.size();j++){
-                res.push_back(tmp[j]);
+        vector<int> res(1,0);
+        for(int i=0;i<n;i++){
+            int curCount=res.size();
+            while(curCount){
+                curCount--;
+                int curNum=res[curCount];//0 1 3 2
+                curNum+=(1<<i);
+                res.push_back(curNum);
             }
         }
-        
         return res;
     }
-    
-    
 };
+//大神还是很多的，代码就是好看
