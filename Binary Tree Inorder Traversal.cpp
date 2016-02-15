510ad75ae80f738958f87ff2c9bc7301484bcc72
Jean <j.z.feng@foxmail.com>
2016-02-16 01:29:29 +0800

    Update Binary Tree Inorder Traversal.cpp

--- Binary Tree Inorder Traversal.cpp
+++ Binary Tree Inorder Traversal.cpp
@@ -15,24 +15,20 @@ public:
     }
 };
 
-
 class Solution2 {
 public:
-    vector<int> inorderTraversal2(TreeNode* root){
-        stack<TreeNode *>st;
-        vector<int> result;
-        
-        while(!(root==NULL&&st.empty())){
-            if(root!=NULL){
-                st.push(root);
-                root=root->left;
-            }else{
-                root=st.top();st.pop();
-                result.push_back(root->val);
-                root=root->right;
-            }//这个时候while循环是不对的，如果只有左子树，就不能出结果。因为有堆栈，可以判断是否为空。
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>res;
+        stack<TreeNode*>st;
+        while(root!=NULL||!st.empty()){
+            while(root!=NULL){
+                st.push(root);root=root->left;
+            }
+            root=st.top();st.pop();
+            res.push_back(root->val);
+            root=root->right;
         }
-        return result;
+        return res;
     }
-    
 };
+//碰到一个非常好的方法
