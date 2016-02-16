0a4c4836ee02dad88190feac81cb7229b3eb6d3d
Jean <j.z.feng@foxmail.com>
2016-02-16 14:14:13 +0800

    Create Interleaving String.cpp

---
+++ Interleaving String.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        /*
+        match[i][j]  means  s1[0...i] s2[0...j]  是否匹配 s3[0...i+j]
+        
+        match[i][j]=(s1[i-1]==s3[i+j-1])&&match[i-1][j]||
+                    (s2[j-1]==s3[i+j-1])&&match[i][j-1])
+        
+        */
+        if(s1.size()+s2.size()!=s3.size())
+            return false;
+        vector< vector<int> >match(s1.size()+1,vector<int>(s2.size()+1,false) );
+        // match[size()+1][size()+1]
+        match[0][0]=true;
+        
+        //初始化
+        for(int i=1;i<=s1.size();i++){
+            if(s1[i-1]==s3[i-1]){
+                match[i][0]=true;
+            }else{
+                break;
+            }
+        }
+        
+        for(int i=1;i<=s2.size();i++){
+            if(s2[i-1]==s3[i-1]){
+                match[0][i]=true;
+            }else{
+                break;
+            }
+        }
+        
+        //初始化结束
+        
+        for(int i=1;i<=s1.size();i++){
+            for(int j=1;j<=s2.size();j++){
+                if(s1[i-1]==s3[i+j-1]){
+                    match[i][j]=match[i-1][j];
+                }
+                if(s2[j-1]==s3[i+j-1]){
+                    match[i][j]=match[i][j-1]||match[i][j];//这里要或以下，但是刚刚不需要。
+                    //因为刚才是第一次，因为公式的原因，他是或的关系。
+                }
+            }
+        }
+        
+        return match[s1.size()][s2.size()];
+    }
+};
