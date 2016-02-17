33624e7a0198dcecf3918fdb88fe378e8ae40b7e
Jean <j.z.feng@foxmail.com>
2016-02-17 21:54:08 +0800

    Update Recover Binary Search Tree.cpp

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -6,21 +6,19 @@ public:
         
         if(prev){
             if(prev->val>root->val){
-                if(n1==NULL){
+                if(n1==NULL)
                     n1=prev;
-                }
                 n2=root;
             }
         }
-        prev=root;
+        prev=root;//可以这样想，前面判断之后，就是继续下一次判断，这个时候root作为下一次的prev了。
         recoverTreeHelper(root->right);
     }
     
     void recoverTree(TreeNode *root) {
         recoverTreeHelper(root);
-        if(n1&&n2){
+        if(n1&&n2)
             swap(n1->val,n2->val);
-        }
     }
 private:
     TreeNode *n1=NULL,*n2=NULL,*prev=NULL;
