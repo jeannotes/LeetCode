48f0bcc5b624fd45e5311b67265a6c7985ef8e0e
Jean <j.z.feng@foxmail.com>
2016-02-19 16:42:13 +0800

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,25 +2,36 @@
 //在网上看了好多的版本，其实这就是动态规划，只不过不要从上而下（对于本题）
 //动归最好从能够理解的那一层次开始，即从最开始的地方的那一层开始.
 //https://leetcode.com/problems/triangle/
-class Solution
-{
+class Solution {
 public:
-	int minimumTotal(vector< vector<int> > &triangle ){
-		if (triangle.empty()){
-			//如果为空，返回1
-			return 0;
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+		vector<vector<int>>dp(m,vector<int>(m,0));
+		for (int i=0;i<m;i++){
+			dp[m-1][i]=triangle[m-1][i];
 		}
-
-		int n=triangle.size();
-		vector <int> row=triangle[n-1];
-
-		for (int i=n-2;i>=0;i--){
-			for (int j=0;j<=n;j++){
-				row[j]=min(row[j]+row[j+1])+triangle[i][j];
+		for (int i=m-2;i>=0;i--){
+			for (int j=0;j<=i;j++){
+				dp[i][j]=min(dp[i+1][j],dp[i+1][j+1])+triangle[i][j];
 			}
 		}
-		return row[0];
-	}
+		return dp[0][0];
+    }
 };
 
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+		vector<int>dp(triangle.back());
+		
+		for (int i=m-2;i>=0;i--){
+			for (int j=0;j<=i;j++){
+				dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
+			}
+		}
+		return dp[0];
+    }
+};
 
+//竟然不会啦
