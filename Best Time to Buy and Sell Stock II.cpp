1fb8c0612fb74d50dad921c3143d4971fbd2ea97
Jean <j.z.feng@foxmail.com>
2016-02-19 22:45:42 +0800

    Update Best Time to Buy and Sell Stock II.cpp

--- Best Time to Buy and Sell Stock II.cpp
+++ Best Time to Buy and Sell Stock II.cpp
@@ -17,3 +17,23 @@ public:
         return sum;
     }
 };
+class Solution2 {
+public:
+//--  更加通俗易懂，贪心法，只要后面一天比前面一天价格高，就卖。然后再买。
+//最简单的算法
+    int maxProfit(vector<int>& prices) {
+		if (prices.empty()){
+			return 0;
+		}
+		int ans=0;
+		int prev=prices[0];
+		for (int i=1;i<prices.size();i++){
+			if (prices[i]>prev){
+				ans+=prices[i]-prev;
+				prev=prices[i];
+			}else
+				prev=prices[i];
+		}
+		return ans;
+	}
+};
