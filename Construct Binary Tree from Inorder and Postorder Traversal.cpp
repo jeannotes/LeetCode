b7406a75370ea162f33d462f9eab5b5e1af2026e
Jean <j.z.feng@foxmail.com>
2016-02-19 17:20:34 +0800

    Update Construct Binary Tree from Inorder and Postorder Traversal.cpp

--- Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -10,31 +10,26 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int posStart=0,preEnd=postorder.size()-1;
-        int inStart=0,inEnd=inorder.size()-1;
-        
-        return constructHelper(postorder,posStart,preEnd,
-                inorder, inStart, inEnd);
-    }
-    
-    TreeNode* constructHelper(vector<int>&postorder,int posStart,int posEnd,
-                vector<int>&inorder,int inStart,int inEnd){
-        if(posStart>posEnd||inStart>inEnd)  return NULL;
-        
-        int val=postorder[posEnd];
-        TreeNode*p =new TreeNode(val);
-        int k=0;
-        for(int i=0;i<inorder.size();i++){
-            if(inorder[i]==val){
-                k=i;break;//第k个
-            } 
-        }
-        
-        p->left= constructHelper(postorder,posStart,posStart+k-inStart-1,inorder, inStart,k-1);//k之前
-                //k-start  考虑这种情况，在找到右子树的左子树的时候
-                //k在右边，inStart也在右边，此时需要找的是左边的个数，
-                //k-inStart 就解决了这个问题
-        p->right=constructHelper(postorder,posStart+k-inStart , posEnd-1,inorder,k+1, inEnd); 
-        return p;        
-    }
-};
+		return constructHelper(postorder,0,postorder.size()-1,inorder,0,inorder.size()-1);
+	}
+	TreeNode* constructHelper(vector<int>& postorder,int postStart,int postEnd,vector<int>& inorder,int inStart,int inEnd){
+		
+		if (postStart>postEnd||inStart>inEnd){
+			return NULL;
+		}
+		int val=postorder[postEnd];
+		int k=0;
+		for (int i=inStart;i<=inEnd;i++){
+			if (inorder[i]==val){
+				k=i;
+				break;
+			}
+		}
+		//45286731 后
+		//42516837 中
+		TreeNode* p=new TreeNode(val);
+		p->left=constructHelper(postorder,postStart,postStart+k-inStart-1,inorder,inStart,k-1);
+		p->right=constructHelper(postorder,postStart+k-inStart,postEnd-1,inorder,k+1,inEnd);
+		return p;
+	}
+};//对照数字，清清楚楚
