d99d06158ddc9bac2cbb03f647fcc92c31d26cb5
Jean <j.z.feng@foxmail.com>
2016-02-20 14:30:37 +0800

    Update Palindrome Partitioning.cpp

--- Palindrome Partitioning.cpp
+++ Palindrome Partitioning.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-//https://leetcode.com/problems/palindrome-partitioning/
-    vector<vector<string>> partition(string s) {
-        vector<string> tmp;
-        vector< vector<string> > result;
-        int length=s.length();
-        if(length==0){
-            return result;
-        }
-        partitionHelper(0,s,tmp,result);
-        return result;
-    }
-    
-    void partitionHelper(int start,string &s,vector<string> &tmp,vector< vector<string> > &result){
-        if(start==s.length()){
-            result.push_back(tmp);
-            return;
-        }
-        
-        for(int i=start;i<s.length();i++){
-            if(isPalindrome(start,i,s)){
-                tmp.push_back(s.substr(start, i-start+1) );//左闭右开
-                partitionHelper(i+1,s,tmp,result);
-                tmp.pop_back();
-            }
-        }
-    }
-    
-    bool isPalindrome(int start,int end,string &s){
-        int mid=(start+end)/2;
-        int i=start,j=end;
-        while(i<=j){
-            if(s[i++]!=s[j--]){
-                return false;
-            }
-        }
-        return true;
-    }
+    vector<vector<string>>result;
+	vector<string>tem;
+	vector<vector<string>> partition(string s) {
+		partitionHelper(s,0);
+		return result;
+	}
+
+	void partitionHelper(string& s,int start){
+		if (start==s.size()){
+			result.push_back(tem);
+			return;
+		}
+		for (int i=start;i<s.size();i++){
+			
+			if (isPalindrome(s,start,i)){
+				string tmp=s.substr(start,i-start+1);
+				tem.push_back(tmp);
+				partitionHelper(s,i+1);
+				tem.pop_back();
+			}
+		}
+	}
+
+	bool isPalindrome(string& s,int start,int end){
+		int i=start,j=end;
+		while (i<=j){
+			if (s[i]!=s[j]){
+				return false;
+			}
+			i++;j--;
+		}
+		return true;
+	}
 };
