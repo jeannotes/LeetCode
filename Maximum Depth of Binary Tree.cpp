830574e8fe9559a1b755b4f413e7df7ea9e48093
Jean <j.z.feng@foxmail.com>
2016-02-27 17:55:53 +0800

    Update Maximum Depth of Binary Tree.cpp

--- Maximum Depth of Binary Tree.cpp
+++ Maximum Depth of Binary Tree.cpp
@@ -1,29 +1,4 @@
  //本质上方法一样，只是通过实践竟然不同
-class Solution1 {
-//https://leetcode.com/problems/maximum-depth-of-binary-tree/
-public:
-    int maxDepth(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        if(root->left==NULL&&root->right==NULL){
-            return 1;
-        }
-        
-        int left=0;
-        if(root->left){
-            left=maxDepth(root->left)+1;
-        }
-        
-        int right=0;
-        if(root->right){
-            right=maxDepth(root->right)+1;
-        }
-        
-        return max(left,right);
-    }
-};
-
 
 class Solution2 {
 public:
