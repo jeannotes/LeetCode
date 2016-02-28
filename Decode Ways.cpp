0c18f43ecad1bcc3d4a3b2be78dd6d2b685d323a
Jean <j.z.feng@foxmail.com>
2016-02-28 22:40:38 +0800

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if(s.empty())   
-            return 0;
-        int len=s.length();
-        int sum[len+1];//sum[i]--s[0...i-1]
-        
-        sum[0]=1;
-        sum[1]=s[0]=='0'?0:1;
-        
-        for(int i=2;i<=len;i++){
-            if(s[i-1]=='0') sum[i]=0;
-            else    sum[i]=sum[i-1];
-            
-            if( s[i-2]=='1'|| (s[i-2]=='2'&&s[i-1]<='6') )
-                sum[i]+=sum[i-2];
-        }
-        return sum[len];
-    }
+		if (s.empty()){
+			return 0;
+		}
+		int len=s.size();
+		vector<int>sum(len+1,0);//sum[i]--s[0...i-1]
+		sum[0]=1;
+		sum[1]=s[0]=='0'?0:1;
+		for (int i=2;i<=len;i++){
+			if (s[i-1]=='0'){
+				sum[i]=0;
+			}else
+				sum[i]=sum[i-1];
+			if ((s[i-2]=='1')||(s[i-2]=='2'&&s[i-1]<='6')){
+				sum[i]+=sum[i-2];
+			}
+		}
+		return sum[len];
+	}
 };
 //sum[0]=1;  12  假如说是12，最后相加的时候必须要这一句，不然加的最前面一个是0
