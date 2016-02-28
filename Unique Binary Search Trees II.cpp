eb5c6a8f4aa50a7025e55c533da32de4093b31c8
Jean <j.z.feng@foxmail.com>
2016-02-28 15:56:52 +0800

    Update Unique Binary Search Trees II.cpp

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -2,7 +2,7 @@ class Solution1 {
 public://https://leetcode.com/problems/unique-binary-search-trees-ii/
     vector<TreeNode*> generateTrees(int n) {
         if(n<1){
-            vector<TreeNode*> dp(NULL);
+            vector<TreeNode*> dp(NULL);//vector<TreeNode*> dp; 写成这个也是可以的
             return dp;
         }
         vector<vector<TreeNode*> >dp(n+1);
@@ -32,31 +32,43 @@ public://https://leetcode.com/problems/unique-binary-search-trees-ii/
     }
 };
 //比之前好多了
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
-        vector<TreeNode*> result;
-        if(n==0)  return result;
-        return help(1, n);
-    }
+		vector<TreeNode*> res;
+		if (n==0){
+			return res;
+		}
+		return treeHelper(1,n);
+	}
 
-    vector<TreeNode*> help(int s, int e){
-        if(s>e)   return vector<TreeNode*>(1, NULL);
-        vector<TreeNode*> result;
-        for(int i=s; i<=e; i++){
-            vector<TreeNode*> left, right;
-            left=help(s, i-1);
-            right=help(i+1, e);
-            for(int m=0; m<left.size(); m++){
-                for(int n=0; n<right.size(); n++){
-                    TreeNode* root=new TreeNode(i);
-                    root->left=left[m];
-                    root->right=right[n];
-                    result.push_back(root);
-                }
-            }
-        }
-        return result;
-    }
+	vector<TreeNode*> treeHelper(int start,int end){
+		if (start>end){
+			return vector<TreeNode*> (1,NULL);
+		}
+		vector<TreeNode*> result;
+		for (int i=start;i<=end;i++){
+			vector<TreeNode*> left,right;
+			left=treeHelper(start,i-1);
+			right=treeHelper(i+1,end);
+			for (int m=0;m<left.size();m++){
+				for (int n=0;n<right.size();n++){
+					result.push_back(new TreeNode(i));
+					result.back()->left=left[m];result.back()->right=right[n];
+				}
+			}
+			
+		}
+		return result;
+	}
 };
 //好题目
