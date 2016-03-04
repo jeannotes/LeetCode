0a96d888f2ee6081c5e6e014504213915cb168c7
Jean <j.z.feng@foxmail.com>
2016-03-04 20:00:50 +0800

    Update Add Binary.cpp

--- Add Binary.cpp
+++ Add Binary.cpp
@@ -41,3 +41,19 @@ public:
         return a;
     }
 };
+
+//最好的程序
+class Solution {
+public:
+    string addBinary(string a, string b) {
+    	string s;
+    	int i=a.length()-1,j=b.length()-1,c=0;
+    	while (i>=0||j>=0||c>0){
+    		c+=i>=0?(int)(a[i--]-'0'):0;
+    		c+=j>=0?(int)(b[j--]-'0'):0;
+    		s=char(c%2+'0')+s;
+    		c=c/2;
+    	}
+    	return s;
+    }
+};
