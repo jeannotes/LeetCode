72dbe66b72256eae2fcf115ddacf0decc79b6dc2
Jean <j.z.feng@foxmail.com>
2016-03-05 09:02:04 +0800

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int dp[m][n];
-        dp[0][0]=1;
-        for(int i=1;i<m;i++){
-            dp[i][0]=dp[i-1][0];
-        }
-        for(int i=1;i<n;i++){
-            dp[0][i]=dp[0][i-1];
-        }
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+    	vector<vector<int>>dp(m,vector<int>(n,1));
+    	for(int i=1;i<m;i++){
+    		for(int j=1;j<n;j++){
+    			dp[i][j]=dp[i-1][j]+dp[i][j-1];
+    		}
+    	}
+    	return dp[m-1][n-1];
     }
 };
 //  还是没有做到bug-free
+// 这个代码更加精简，学会使用 vector
