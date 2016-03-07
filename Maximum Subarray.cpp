4577acb0debe9cdc8fbd79b72bb889ab01b59762
Jean <j.z.feng@foxmail.com>
2016-03-07 14:30:33 +0800

    Update Maximum Subarray.cpp

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -2,51 +2,48 @@ class Solution {
 public:
 //http://www.tuicool.com/articles/IbiMjaI
     int maxSubArray(vector<int>& nums) {
-        int sum=nums[0],res=nums[0];
-        for(int i=1;i<nums.size();i++){
-            sum=sum<0?(nums[i]):(sum+nums[i]);
-            res=max(res,sum);
-        }
-        return res;
+    	int sum=nums[0],res=nums[0];
+    	for (int i=1;i<nums.size();i++){
+    		if (sum<0){
+    			sum=0;
+    		}
+    		sum+=nums[i];
+    		res=max(res,sum	);
+    	}
+    	return res;
     }
 };
 
-class Solution {
+class Solution2 {
 public:
-    int recursiveMaxSubArray(vector<int>&A, int left, int right)
-    {
-    	if(left == right)return A[left];
-    	if(left == right-1)return max(max(A[left], A[right]), A[left]+A[right]);
-    
-    	//divide
-    	int mid = (left + right)/2;
-    	int lmax = recursiveMaxSubArray(A, left, mid-1);
-    	int rmax = recursiveMaxSubArray(A, mid+1, right);
-    
-    	//the max is [i..mid..j]
-    	int mmax = A[mid];
-    
-    	for(int j = mid + 1, sum = mmax; j <= right; ++j)
-    	{
-    		sum += A[j];
-    		if(sum > mmax)
-    			mmax = sum;	
-    	}
-    	
-    	for(int j = mid - 1, sum = mmax; j >= left; --j)
-    	{
-    		sum += A[j];
-    		if(sum > mmax)
-    			mmax = sum;
-    	}
-    
-    	return max(max(lmax, rmax), mmax);
+    int maxSubArray(vector<int>& nums) {
+    	return maxSubHelper(nums,0,nums.size()-1);
     }
     
-    int maxSubArray(vector<int>& nums) 
-    {
-    	return recursiveMaxSubArray(nums, 0, nums.size()-1);
-    }
+    int maxSubHelper(vector<int>&nums,int left,int right){
+    	if (left==right){
+    		return nums[left];
+    	}
+    	if (left==right-1){
+    		return max( max(nums[left],nums[right]),nums[left]+nums[right] );
+    	}
+    	int mid=(left+right)/2;
+    	int lmax=maxSubHelper(nums,left,mid-1);
+    	int rmax=maxSubHelper(nums,mid+1,right);
+    
+    	int nmax=nums[mid];
+    	for (int j=mid+1,sum=nums[mid];j<=right;j++){
+    		sum+=nums[j]; //这个地方不写sum=max(sum,sum+nums[i])的原因是假设计算到第6位数字，而此时前三位最大，再加上第7位数字
+    		                //就是前面三位加上第7位，就不是连续计算的。
+    		nmax=max(nmax,sum);
+    	}
+    	for (int j=mid-1,sum=nmax;j>=left;--j){
+    		sum+=nums[j];
+    		nmax=max(nmax,sum);
+    	}
+    	return max( max(lmax,rmax),nmax  );
+    }	
+
 };
 //  假设前面k个是sum，第k+1个的时候，如果sum小于0，对我后面计算是没有好处的，但是前提是之前的最大和已经保存在maxSum里面啦。
 //所以会和前面的去比较的，没事
