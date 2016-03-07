09e09562c9f30d19cad6d7cb78f814111b8b5880
Jean <j.z.feng@foxmail.com>
2016-03-07 15:09:50 +0800

    Update Pow(x, n).cpp

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0)
-            return 1;
-        if(x==0||x==1)
-            return x;
-        if(n<0){
-            if(n==INT_MIN)
-                return 1;
-            return myPow(1.0/x,-n);
-        }
-        return n%2==0? myPow(x*x,n/2):x*myPow(x*x,n/2);   
+    	if (x==0){
+    		return x;
+    	}
+    	if (n==0){
+    		return 1;
+    	}
+    
+    	if (n<0){
+    	    if(n==INT_MIN)
+    	    	return 1.0/(myPow(x,INT_MAX)*x);
+    	    else    
+    	        return myPow(1.0/x,-n);
+    	}
+    
+    	return n%2==0?(myPow(x*x,n/2)):(myPow(x*x,n/2)*x);
     }
 };
-//还是不会，究其原因，可以这样理解不要考虑正向最大值，因为会不断的除以2，但是需要考虑的是
-//当n为负数无穷大，为什么结果是1
+//用位运算代替乘除和求余优化效率：移位代替除法（n>>1 == n /2);用位与代替求余判定奇偶（if (n& 0x1 == 1) == if(n % 2 == 1))
+
+//对于n取值INT_MIN = -2147483648时，-n依然是INT_MIN，并不是INT_MAX=2147483647，这时需要格外小心。
