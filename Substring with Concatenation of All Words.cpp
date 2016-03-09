4f1385ba6211e2b7a07a11b7d8dad85b579054c9
Jean <j.z.feng@foxmail.com>
2016-03-09 22:08:51 +0800

    Update Substring with Concatenation of All Words.cpp

--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -2,37 +2,49 @@ class Solution {
 //https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 //本题主要还是使用滑块法，就是从左往右依次滑动，然后检查这个位置是否存在
 public:
-    vector<int> findSubstring(string S, vector<string> &L) {
-        vector<int> res;
-        if(L.empty())   return res;
-        int totalWords=L.size();
-        int wordSize=L[0].size();
-        int totalLen=wordSize*totalWords;
-        if(S.size()<totalLen)   return res;//s自身字符串没有给出的表里面的总和字符数目多，没有任何输出
-        unordered_map<string,int> wordCount;
-        for(int i=0;i<L.size();i++){
-            wordCount[L[i]]++;
-        }
-        
-        for(int i=0;i<=S.size()-totalLen;i++){
-            if(checkSubstring( S , i , wordCount , wordSize , totalWords))
-                res.push_back(i);
-        }
-        return res;
+       vector<int> findSubstring(string s, vector<string>& words) {
+    	vector<int>res;
+    	if (s.empty()){
+    		return res;
+    	}
+    	int totalWords=words.size();
+    	int wordSize=words[0].size();
+    	int totalLen=totalWords*wordSize;
+    
+    	if (s.size()<totalLen){
+    		return res;
+    	}
+    
+    	unordered_map<string,int>wordCount;
+    	for (int i=0;i<totalWords;i++){
+    		wordCount[words[i]]++;
+    	}
+    
+    	for (int i=0;i<=s.length()-totalLen;i++){
+    		if (checkSubstring(s,i,wordCount,wordSize,totalWords)){
+    			res.push_back(i);
+    		}
+    	}
+    	return res;
     }
     
-    bool checkSubstring(string &S,int start,unordered_map<string,int>&wordCount,int wordSize,int totalWords){
-        if(S.size()-start+1<totalWords*wordSize)    return false;
-        unordered_map<string,int> wordFound;
-        
-        for(int i=0;i<totalWords;i++){
-            string tmp=S.substr(start+i*wordSize,wordSize); 
-            if(  !wordCount.count(tmp)    )
-                return false;//首先一开始我找的wordSize个字符如果不是hash表里面的直接返回
-            wordFound[tmp]++;                                             
-            if( wordFound[tmp] > wordCount[tmp] )   return false;                                          
-        }
-        return true;
+    bool checkSubstring(string& s,int start,unordered_map<string,int>& wordCount,int wordSize
+    	,int totalWords){
+    	if (s.size()-start+1<wordSize*totalWords){
+    		return false;
+    	}
+    	unordered_map<string,int>wordFound;
+    	for (int i=0;i<totalWords;i++){
+    		string tmp=s.substr(start+i*wordSize,wordSize);
+    		if (!wordCount.count(tmp)){
+    			return false;
+    		}
+    		wordFound[tmp]++;
+    		if (wordFound[tmp]>wordCount[tmp]){
+    			return false;
+    		}
+    	}
+    	return true;
     }
     
 };
