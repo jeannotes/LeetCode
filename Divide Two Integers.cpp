4d79e61a738f1e0b47dc431f17cbadba8af243b1
Jean <j.z.feng@foxmail.com>
2016-03-10 10:45:57 +0800

    Update Divide Two Integers.cpp

--- Divide Two Integers.cpp
+++ Divide Two Integers.cpp
@@ -1,26 +1,21 @@
-typedef long long ll;
 class Solution {
 public:
-//https://leetcode.com/problems/divide-two-integers/
+    typedef long long ll;
     int divide(int dividend, int divisor) {
-        ll a = dividend >= 0 ? dividend : -(ll)dividend;
-        ll b = divisor >= 0 ? divisor : -(ll)divisor;
-        ll result = 0, c = 0;
-        bool sign = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
-
-        while(a>=b){
-            c=b;
-            for(int i=0;a>=c;i++,c<<=1){
-                a=a-c;
-                result=result+(1<<i);
-            }
-            //c<<suoy=1,每一次这个操作就会使得除数*2。因为他是迭代的，
-            //所以循环内部是+(1<<i)-->代表多少个倍数的除数。
-        }
-        if (sign) {//max 函数需要保证后面的数字是同一个类型的
-            return max((ll)INT_MIN, -result);
-        } else {
-            return min((ll)INT_MAX, result);
-        }
+        if (!divisor || (dividend == INT_MIN && divisor == -1))
+            return INT_MAX;
+        
+    	ll a = llabs(dividend);
+    	ll b = llabs(divisor);
+    	bool sign=(dividend>0&&divisor<0)||(dividend<0&&divisor>0);
+    	ll result=0,c=b;
+    	while (a>=b){
+    		c=b;
+    		for (int i=0;a>=c;c=c<<1,i++){
+    			a=a-c;
+    			result+=(1<<i);
+    		}
+    	}
+    	return sign==true? -result:result;
     }
 };
