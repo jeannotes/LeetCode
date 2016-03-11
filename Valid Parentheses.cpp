201aaeea899511d9f682f446a4c22e2c3ae54c51
Jean <j.z.feng@foxmail.com>
2016-03-11 10:45:38 +0800

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.empty())
-            return true;
-        stack<char>res;
-        if(s.length()%2!=0)
-            return false;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('||s[i]=='{'||s[i]=='[')
-                res.push(s[i]);
-            else{
-                if(res.empty())
-                    return false;
-                else{
-                    char tem=res.top();res.pop();
-                    if(tem=='('&&s[i]==')'||tem=='['&&s[i]==']'||tem=='{'&&s[i]=='}')
-                        continue;
-                    else
-                        return false;
-                }
-            }
-        }
-        return res.empty();//防止出现 “((”的情况
+    	if (s.empty()){
+    		return true;
+    	}
+    
+    	int len=s.size();
+    	if (len%2!=0){
+    		return false;
+    	}
+    	stack<char>res;
+    	for (int i=0;i<len;i++){
+    		if (s[i]=='('||s[i]=='['||s[i]=='{'){
+    			res.push(s[i]);
+    		}else{
+    			if (res.empty()){
+    				return false;
+    			}else{
+    				char c=res.top();res.pop();
+    				if((c=='('&&s[i]!=')')||(c=='['&&s[i]!=']')||(c=='{'&&s[i]!='}')){
+    					return false;
+    				}  
+    			}
+    		}
+    	}
+    	return res.empty();
     }
 };
+//防止出现 “((”的情况
+//本题颇有难度，	if((c=='('&&s[i]!=')')||(c=='['&&s[i]!=']')||(c=='{'&&s[i]!='}'))  这个判断不曾想到
+//还有最后的 “((”的情况 也不曾想到
