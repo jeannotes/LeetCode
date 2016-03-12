e4989f7890c56abe0fe0d2f18d9bd280d1ecb8a6
Jean <j.z.feng@foxmail.com>
2016-03-12 18:34:15 +0800

    Update String to Integer (atoi).cpp

--- String to Integer (atoi).cpp
+++ String to Integer (atoi).cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-//https://leetcode.com/problems/string-to-integer-atoi/
     int myAtoi(string str) {
-        int result=0;
-        
-        int i=0,sign=1;
-        int length=str.length();
-        
-        while(str[i]==' '){
-            i++;
-        }//找到非空值
-        
-        if(str[i]=='+'){
-            sign=1;
-            i++;
-        }else if(str[i]=='-'){//完美解决没有符号问题
-            sign=-1;
-            i++;
-        }
-        
-        while(   '0'<=str[i]&&str[i]<='9'   ){
-            if(result>INT_MAX/10){
-                return sign==1?INT_MAX:INT_MIN;
-            }
-            if(result==INT_MAX/10){
-                if(sign==1){
-                    return str[i]>'7'?(INT_MAX):(result*10+(str[i]-'0'));
-                }
-                if(sign==-1){
-                    return str[i]>='8'?(INT_MIN):-(result*10+(str[i]-'0'));
-                }
-            }
-            result=result*10+(str[i]-'0');
-            i++;
-        }
-        
-        return result*sign;
+    	if (str.empty()){
+    		return 0;
+    	}
+    	int sign=1,i=0,base=0;
+    	while (str[i]==' '){
+    		i++;
+    	}
+    	if (str[i]=='-'||str[i]=='+'){
+    		sign=1-2*(str[i++]=='-');
+    	}
+    	while ('0'<=str[i]&&str[i]<='9'){
+    		if (base>INT_MAX/10||(base==INT_MAX/10&&str[i]>'7')){
+    			if (sign==1){
+    				return INT_MAX;
+    			}else
+    				return INT_MIN;
+    		}
+    		base=base*10+(str[i++]-'0');
+    	}
+    	return base*sign;
     }
 };
+// 好题目啊
+// 不会
