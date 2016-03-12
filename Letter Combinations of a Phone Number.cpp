623083be65636ebb38b7d85b411033473105801f
Jean <j.z.feng@foxmail.com>
2016-03-12 08:45:11 +0800

    Update Letter Combinations of a Phone Number.cpp

--- Letter Combinations of a Phone Number.cpp
+++ Letter Combinations of a Phone Number.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-//https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+    vector<string> res;
+    string tmp;
     vector<string> letterCombinations(string digits) {
-        string table[]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};//  (number:2-9)==(0-7)
-        vector<string> result;
-        if(digits.empty()){     //考虑输入为空的情况
-            return result;   
-        }
-        string s;//缓存
-        combinationHelper(table,digits,0,s,result);//table[]不对
-        return result;
+    	if (digits.empty()){
+    		return res;
+    	}
+    	string table[]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    	letterHelper(0,digits,table);
+    	return res;
     }
     
-    void combinationHelper(string table[],string &digits,int start,string &s,vector<string> &result){
-        if(start==digits.size()){
-            result.push_back(s);
-            return;
-        }//限制条件
-        
-        int i=digits[start]-'2';
-        if(i<0||i>7){//这一段没有也没事
-            return;//本来以为这句是没啥用的，只是后面start不断+1,会导致溢出，所以有用。
-        }
-        
-        for(int j=0;j<table[i].size();j++){
-            s.push_back(table[i][j]);
-            combinationHelper(table,digits,start+1,s,result);
-            s.pop_back();
-        }
+    void letterHelper(int start,string &digits,string table[]){
+    	if (start==digits.size()){
+    		res.push_back(tmp);
+    		return;
+    	}
+    	int p=digits[start]-'2';
+    	for (int i=0;i<table[p].size();i++){
+    		tmp+=table[p][i];
+    		letterHelper(start+1,digits,table);
+    		tmp.pop_back();
+    	}
     }
 };
+//代码更加简洁
