f202439fd44477f83335a5d0154780deb17f1d83
Jean <j.z.feng@foxmail.com>
2016-03-14 00:29:58 +0800

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-//https://leetcode.com/problems/reverse-integer/
     int reverse(int x) {
-        if(x<0){
-            return x==INT_MIN?0:-reverse(-x);
-        }
-        
-        int y=0,res=0;
-        // 123--321
-        while(x>0){
-            if(res>INT_MAX/10)
-                return 0;
-            if(res==INT_MAX&&(x%10>7))
-                return 0;
-            res=res*10+x%10;
-            x=x/10;
-        }
-        return res;
+    	if (x<0){
+    		return x==INT_MIN?0:-reverse(-x);
+    	}
+    	int res=0;
+    	while (x>0){
+    		if (res>INT_MAX/10||(res>INT_MAX/10&&x>7)){
+    			return 0;
+    		}
+    		res=res*10+x%10;
+    		x=x/10;
+    	}
+    	return res;
     }
 };
