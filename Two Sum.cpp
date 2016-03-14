41f62918ebc2c81088dc4e3e07bddbff4c81eece
Jean <j.z.feng@foxmail.com>
2016-03-14 16:55:18 +0800

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,18 +1,21 @@
-vector<int> twoSum(vector<int>& nums, int target) {
-	vector<int>res;
-	if (nums.empty()){
-		return res;
-	}
-	unordered_map<int,int>number;
-	for (int i=0;i<nums.size();i++){
-		int tmp=target-nums[i];
-		if (number.find(tmp)!=number.end()){
-			res.push_back(number[tmp]);
-			res.push_back(i);
-			break;
-		}else
-			number[nums[i]]=i;
-	}
-	return res;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    	vector<int>res;
+    	if (nums.empty()){
+    		return res;
+    	}
+    	unordered_map<int,int>mp;
+    	for (int i=0;i<nums.size();i++){
+    		int tmp=target-nums[i];
+    		if (mp.find(tmp)!=mp.end()){
+    			res.push_back(mp[tmp]);res.push_back(i);
+    			break;
+    		}else
+    			mp[nums[i]]=i;
+    	}
+    	return res;
+    }
+};
 //题目不难，使用hash表，再次强调，学习STL，把几个map的函数学会啊
+//不太会
