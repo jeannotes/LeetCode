dd9a3f84cf491948b67360e6c69513798ba4c3b0
Jean <j.z.feng@foxmail.com>
2016-03-17 17:56:28 +0800

    Update Populating Next Right Pointers in Each Node.cpp

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -31,7 +31,7 @@ public:
 		if (root==NULL){
 			return;
 		}
-		if (root->left&&root->right){
+		if (root->left){
 			root->left->next=root->right;
 		}
 		if (root->right&&root->next){
@@ -41,3 +41,9 @@ public:
 	}
 };
 //值得庆祝的一天，递归写出来了
+
+//还是有许多问题啊
+
+//第二种方法：if (root->right&&root->next) 按照道理不写左边的root->right没事，
+//因为第二句root->left存在，那么root->right一定存在
+//但是到最底层，最左边的时候，root->next是存在的，只是当前的root->right->next不存在
