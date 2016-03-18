4dc57b9fef9d517afb78981308fe5023d68dc4b5
Jean <j.z.feng@foxmail.com>
2016-03-18 17:37:22 +0800

    Update Binary Tree Postorder Traversal.cpp

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -1,43 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution1 {
+class Solution {
 public:
-//http://blog.csdn.net/jsrgfjz/article/details/48696527
-//https://leetcode.com/problems/binary-tree-postorder-traversal/
-    vector<int> postorderTraversal1(TreeNode *root) {
-    vector<int> result;
-    vector<TreeNode*> stack;
-    TreeNode *node=root;
-    TreeNode *lastVisitNode=NULL;
+    vector<int> postorderTraversal(TreeNode* root) {
+    	vector<int>res;
+    	stack<TreeNode*>stk;
     
-    while(node!=NULL||stack.size()>0){
-        if(node!=NULL){
-            stack.push_back(node);
-            node=node->left;
-        }else{
-            TreeNode *n=stack.back();
-            if(n->right==NULL||lastVisitNode==n->right){
-                //如果为空，访问根节点，如刚刚访问右节点，访问根节点。
-                result.push_back(n->val);
-                stack.pop_back();
-                lastVisitNode=n;
-            }
-            else{
-                //访问的左节点,没有关系，反正最后还是会转移到这个节点
-                node=n->right;
-            }
-        }
+    	if (root==NULL){
+    		return res;
+    	}
+    	TreeNode* last=NULL;
+    	while (root!=NULL||!stk.empty()){
+    		if (root!=NULL){
+    			stk.push(root);
+    			root=root->left;
+    		}else{
+    			TreeNode* n=stk.top();
+    			if (n->right==NULL||last==n->right){
+    				res.push_back(n->val);//如果为空，访问根节点，如刚刚访问右节点，访问根节点。
+    				stk.pop();
+    				last=n;
+    			}else{
+    				root=n->right;
+    			}
+    		}
+    	}
+    	return res;
     }
-    return result;
-}
 };
+
 
 class Solution2 {
 public:
