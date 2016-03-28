a63efc2d4ef113190c359b4a1589ff4120f2db58
Jean <j.z.feng@foxmail.com>
2016-03-28 17:20:42 +0800

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -23,16 +23,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
-		vector<int>dp(triangle.back());
-		
-		for (int i=m-2;i>=0;i--){
-			for (int j=0;j<=i;j++){
-				dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
-			}
+	vector<int>dp(triangle.back());
+	
+	for (int i=m-2;i>=0;i--){
+		for (int j=0;j<=i;j++){
+			dp[j]=min(dp[j],dp[j+1])+triangle[i][j];
 		}
-		return dp[0];
+	}
+	return dp[0];
     }
 };
 
 //竟然不会啦
 //还是会有很大问题啊，怎么这题不会呢
+//没有问题
