027d0ee8a5b79d0d4e6c3f68fed694c9dacf7534
Jean <j.z.feng@foxmail.com>
2016-03-30 16:28:22 +0800

    Update Search for a Range.cpp

--- Search for a Range.cpp
+++ Search for a Range.cpp
@@ -35,3 +35,46 @@ public:
 };
 //  bug  在大于等于和小于等于方面存在问题
 //  如果找不到的话是 i>j的，要找到这个规律
+
+
+//迭代的版本
+class Solution {
+public:
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2,-1);
+    	if (nums.empty()||target<nums[0]||target>nums[nums.size()-1]){
+    		return res;
+    	}
+    	int i=searchLow(nums,target);
+    	int j=searchHigh(nums,target);
+    	if (i<=j){//123 
+    		res[0]=i;res[1]=j;
+    	}
+    	return res;
+    }
+    
+    int searchLow(vector<int>& nums, int target){
+    	int i=0,j=nums.size()-1;
+    	while (i<=j){
+    		int mid=(i+j)/2;
+    		if (nums[mid]>=target){
+    			j=mid-1;
+    		}else
+    			i=mid+1;//1223
+    	}
+    	return i;
+    }
+    
+    int searchHigh(vector<int>& nums, int target){
+    	int i=0,j=nums.size()-1;
+    	while (i<=j){
+    		int mid=(i+j)/2;
+    		if (nums[mid]<=target){
+    			i=mid+1;
+    		}else
+    			j=mid-1;//123
+    	}
+    	return j;
+    }
+};
