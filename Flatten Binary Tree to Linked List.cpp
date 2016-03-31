319e5d947282c2b1fe1dc823188283554a70dd06
Jean <j.z.feng@foxmail.com>
2016-03-31 09:49:16 +0800

    Update Flatten Binary Tree to Linked List.cpp

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -36,3 +36,28 @@ public:
 	}
 };
 // 依然有一丢丢问题
+class Solution {
+public:
+    vector<TreeNode*>res;
+    void flatten(TreeNode* root) {
+    	if (root==NULL){
+    		return;
+    	}
+    	pregenerateTreeNode(root);
+    	for (int i=1;i<res.size();i++){
+    		root->left=NULL;root->right=res[i];
+    		root=root->right;
+    	}
+    	root->left=NULL;root->right=NULL;
+    }
+    
+    void pregenerateTreeNode(TreeNode* root){
+    	if (root==NULL){
+    		return;
+    	}
+    	res.push_back(root);
+    	pregenerateTreeNode(root->left);
+    	pregenerateTreeNode(root->right);
+    }
+};
+//这条题目有问题，上面的答案能够通过！！！？
