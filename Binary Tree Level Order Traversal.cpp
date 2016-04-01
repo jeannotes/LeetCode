89195fc24eee068cfc126a81d69351d1dfce5f43
Jean <j.z.feng@foxmail.com>
2016-04-01 10:53:54 +0800

    Update Binary Tree Level Order Traversal.cpp

--- Binary Tree Level Order Traversal.cpp
+++ Binary Tree Level Order Traversal.cpp
@@ -24,26 +24,4 @@ public:
 };
 //不能捡了西瓜丢了芝麻
 // int n=q.size(); 这句话不能少
-
-class Solution {
-public:
-    vector<vector<int> > res;
-
-    void DFS(TreeNode* root, int level)
-    {
-        if (root == NULL) return;
-        if (level == res.size()) // The level does not exist in output
-        {
-            res.push_back(vector<int>()); // Create a new level
-        }
-    
-        res[level].push_back(root->val); // Add the current value to its level
-        DFS(root->left, level+1); // Go to the next level
-        DFS(root->right,level+1);
-    }
-    
-    vector<vector<int> > levelOrder (TreeNode *root) {
-        DFS(root, 0);
-        return res;
-    }
-};
+// int n=q.size(); 这句话不能少
