84796cea0bc72c4962b05a9241036b5b73064712
Jean <j.z.feng@foxmail.com>
2016-04-04 11:25:22 +0800

    Update Wildcard Matching.cpp

--- Wildcard Matching.cpp
+++ Wildcard Matching.cpp
@@ -100,3 +100,28 @@ bool isMatch(string s, string p) {
 }
 // 做了好多遍了，还是不会啊
 //   还是很大问题啊
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+    	int m=s.size(),n=p.size();
+    	vector<vector<bool>>dp(m+1,vector<bool>(n+1,false));
+    	dp[0][0]=true;
+    	//dp[i][j]...s[0,,i-1]  p[0..j-1]
+    	for (int i=1;i<=n;i++){
+    		if (p[i-1]!='*')	break;
+    		dp[0][i]=p[i-1]=='*'&&dp[0][i-1];
+    	}
+    
+    	for (int i=1;i<=m;i++){
+    		for (int j=1;j<=n;j++){
+    			if (s[i-1]==p[j-1]||p[j-1]=='?')
+    				dp[i][j]=dp[i-1][j-1];
+    			if (p[j-1]=='*'){
+    				dp[i][j]=dp[i][j-1]||dp[i-1][j];
+    			}
+    		}
+    	}
+    	return dp[m][n];
+    }
+};
+//这是自己写的，加油啊
