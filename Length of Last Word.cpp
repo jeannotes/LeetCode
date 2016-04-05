ded76338c545fa718ea492f872b89519f4dec382
Jean <j.z.feng@foxmail.com>
2016-04-05 15:46:54 +0800

    Update Length of Last Word.cpp

--- Length of Last Word.cpp
+++ Length of Last Word.cpp
@@ -2,19 +2,18 @@ class Solution {
 //https://leetcode.com/problems/length-of-last-word/
 public:
     int lengthOfLastWord(string s) {
-        if(s.empty())
-            return 0;
-        int i=s.length()-1,count=0;
-        while(i>=0&&s[i]==' ')
-            i--;
-        for(;i>=0;i--){
-            if(s[i]!=' ')
-                count++;
-            else
-                break;
-        }
-        return count;
+    	if (s.empty())
+    		return 0;
+    	int j=s.size()-1,count=0;
+    	while (s[j]==' '&&j>=0){
+    		j--;
+    	}
+    	while (s[j]!=' '&&j>=0){
+    		count++;j--;
+    	}
+    	return count;
     }
 };
 // bug-free
 //基本没问题
+//竟然有问题啊
