bee62704b8182a51dcd57e655c14361e105a184f
Jean <j.z.feng@foxmail.com>
2016-04-26 21:04:42 +0800

    Update Container With Most Water.cpp

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -27,3 +27,26 @@ public:
 // 不太会做啊
 //  左边的大，右边的往左走，左边的小，左边的往右走
 //还是不会啊
+/*
+思路：
+
+由于ai和aj (i<j) 组成的container的面积：S(i,j) = min(ai, aj) * (j-i)
+
+所以对于任何S(i'>=i, j'<=j) >= S(i,j)，由于j'-i' <= j-i，必然要有min(ai',aj')>=min(ai,aj)才行。同样可以采用头尾双指针向中间移动：
+
+当a(left) < a(right)时，对任何j<right来说
+
+(1) min(a(left),aj) <= a(left) = min(a(left), a(right))
+(2) j-left < right-left
+
+所以S(left, right) > S(left, j<right)。排除了所有以left为左边界的组合，因此需要右移left。同理，当a(left) > a(right)时，需要左移right。而当a(left) = a(right)时，需要同时移动left和right。
+
+思路整理：
+left = 0, right = n-1
+(1) a[left] < a[right], left++
+(2) a[left] > a[right], right--
+(3) a[left] = a[right], left++, right--
+终止条件：left>-right
+
+
+*/
