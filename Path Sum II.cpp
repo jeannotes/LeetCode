235507d31ea571bb355098ad7fefb2e52643e56d
Jean <j.z.feng@foxmail.com>
2016-05-18 22:13:11 +0800

    Update Path Sum II.cpp

--- Path Sum II.cpp
+++ Path Sum II.cpp
@@ -1,28 +1,25 @@
 class Solution {
-//https://leetcode.com/problems/path-sum-ii/
 public:
+    vector<vector<int>>res;
+    vector<int>tem;
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<int> temp;
-        vector< vector<int> >result;
-        pathSumHelper(root,sum,temp,result);
-        return result;
-        //好久时间
+    	pathHelper(root, sum);
+    	return res;
     }
     
-    void pathSumHelper(TreeNode *root,int sum,vector<int> &temp,vector<vector<int> >&result){
-        if(root==NULL){
-            return;
-        }
-        temp.push_back(root->val);
-        if(root->left==NULL&&root->right==NULL&&root->val==sum){
-            result.push_back(temp);//这个地方不能够加return //0 1 1 sum=1 加上return 返回 01 011
-        }
-        pathSumHelper(root->left,sum - root->val,temp,result);
-        pathSumHelper(root->right,sum - root->val,temp,result);
-        
-        temp.pop_back();
+    void pathHelper(TreeNode* root,int sum) {
+    	if (root == NULL)
+    		return;
+    	tem.push_back(root->val);
+    	if (root->left == NULL&&root->right== NULL&&sum == root->val)
+    		res.push_back(tem);//这个地方不能够加return //0 1 1 sum=1 加上return 返回 01 011
+    	else {
+    		pathHelper(root->left,sum-root->val);
+    		pathHelper(root->right, sum - root->val);
+    	}
+    	tem.pop_back();
     }
 };
 //其实我还是不会
 //几号，回溯中间不能有return
-//还好，基本一次通过
+//不会啊，回溯中间不能够有return的
