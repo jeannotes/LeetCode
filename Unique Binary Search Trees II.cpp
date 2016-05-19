d60b0913e8c2df5326e8f5bc2b4d16031325e4e7
Jean <j.z.feng@foxmail.com>
2016-05-19 08:49:08 +0800

    Update Unique Binary Search Trees II.cpp

--- Unique Binary Search Trees II.cpp
+++ Unique Binary Search Trees II.cpp
@@ -1,34 +1,35 @@
 class Solution1 {
-public://https://leetcode.com/problems/unique-binary-search-trees-ii/
+public:
+    vector<TreeNode*>res;
     vector<TreeNode*> generateTrees(int n) {
-        if(n<1){
-            vector<TreeNode*> dp(NULL);//vector<TreeNode*> dp; 写成这个也是可以的
-            return dp;
-        }
-        vector<vector<TreeNode*> >dp(n+1);
-        dp[0].push_back(NULL);
-        dp[1].push_back(new TreeNode(1));
-        
-        for(int i=2;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                for(int k=0;k<dp[j-1].size();k++){
-                    for(int w=0;w<dp[i-j].size();w++){
-                        dp[i].push_back(new TreeNode(j));
-                        dp[i].back()->left=dp[j-1][k];
-                        traverse_add_copy(dp[i-j][w],dp[i].back()->right,j);
-                    }
-                }
-            }
-        }
-        return dp[n];
+    	if (n == 0)
+    		return res;
+    	vector<vector<TreeNode*> >dp(n + 1);
+    	dp[0].push_back(NULL);
+    	dp[1].push_back(new TreeNode(1));
+    
+    	for (int i = 2; i <= n;i++) {
+    		for (int j = 1; j <= i;j++) {
+    			for (int k = 0; k < dp[j - 1].size();k++) {
+    				for (int l = 0; l < dp[i - j].size();l++) {
+    					dp[i].push_back(new TreeNode(j));//123 4 56
+    					dp[i].back()->left = dp[j - 1][k];
+    					traverse_copy(dp[i-j][l], dp[i].back()->right, j);
+    				}
+    			}
+    		}
+    	}
+    	return dp[n];
     }
-
-    void traverse_add_copy(TreeNode *&root,TreeNode *&dst,int val){
-        if(root==NULL)  return;
-        dst=new TreeNode(root->val+val);
-        
-        traverse_add_copy(root->left,dst->left,val);
-        traverse_add_copy(root->right,dst->right,val);
+    
+    void traverse_copy(TreeNode* &root, TreeNode* &dst, int val) {
+    	if (root == NULL)
+    		return;
+    	dst = new TreeNode(root->val + val);
+    	if (root->left)
+    		traverse_copy(root->left, dst->left, val);
+    	if (root->right)
+    		traverse_copy(root->right, dst->right, val);
     }
 };
 //比之前好多了
@@ -75,6 +76,4 @@ public:
 //   vector<TreeNode*> dp(NULL);  这个地方  写成vector<TreeNode*> dp(1，NULL);就会出错
 //来总结一下 ：第一个方法是这样，vector<TreeNode*> dp(1，NULL);最后返回 “[[]]”，因为中间是一个null
 //而第二个方法是 : 如果写成return vector<TreeNode*> (NULL);这个没有大小，后面left[i]反而没有意义了
-//还是要多加练习的，学习如逆水行舟啊，不进则退
-//还是不会啊，还是不很熟啊，就是  返回那边出问题啊
-//返回那边还有问题，递归不会了啊
+//动态规划一次通过，递归不会
