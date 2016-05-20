8ef36c1e6c836bd7cca91c2c1a9643cec6859922
Jean <j.z.feng@foxmail.com>
2016-05-20 09:57:12 +0800

    Update Distinct Subsequences.cpp

--- Distinct Subsequences.cpp
+++ Distinct Subsequences.cpp
@@ -24,3 +24,22 @@ public:
 // 之前理解有误，单从字面理解，可以想为，s中字符删减多少次变成t中的字符
 //但写代码的时候还是不能够按照这种方法来理解，只是由给的列子来看，还是使用删减法来理解最好不过
 //还是有问题
+class Solution2 {
+public:
+    int numDistinct(string s, string t) {
+    	int m = s.size(), n = t.size();
+    	vector<vector<int>>path(m + 1, vector<int>(n + 1,0));
+    	for (int i = 0; i <= m;i++) {
+    		path[i][0] = 1;
+    	}
+    	for (int i = 1; i <= m;i++) {
+    		for (int j = 1; j <= n;j++) {
+    			path[i][j] = path[i - 1][j] + (s[i - 1] == t[j - 1] ? path[i - 1][j - 1] : 0);
+    		}
+    	}
+    	return path[m][n];
+    }
+};
+// https://leetcode.com/discuss/2143/any-better-solution-that-takes-less-than-space-while-in-time
+// 不是很理解，只是可以这样想，从i到j无论怎样，可以删除当前的第i-1个，
+//因此一定会加上dp[i-1][j]，然后就是第i个是不是等于第j个
