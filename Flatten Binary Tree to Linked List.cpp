24c4749e42d0ab89fb59436983aa2735dcde8aa5
Jean <j.z.feng@foxmail.com>
2016-05-20 08:29:43 +0800

    Update Flatten Binary Tree to Linked List.cpp

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -1,63 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution {
+class Solution1 {
 public:
-	vector<TreeNode*>res;
-	void flatten(TreeNode* root) {
-		if (root==NULL){
-			return;
-		}
-		preorderHelper(root);
-		TreeNode* dummy=root;
-		
-		for (int i=0;i<res.size();i++){
-			root->left=NULL;
-			root->right=res[i];
-			root=root->right;
-		}
-		root->left=NULL;
-		root->right=NULL;
-		root=dummy;
-	}
-	void preorderHelper(TreeNode* root){
-		if (root==NULL){
-			return;
-		}
-		res.push_back(root);
-		preorderHelper(root->left);
-		preorderHelper(root->right);
-	}
-};
-// 依然有一丢丢问题
-class Solution {
-public:
-    vector<TreeNode*>res;
+    TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
-    	if (root==NULL){
+    	if (root == NULL)
     		return;
-    	}
-    	pregenerateTreeNode(root);
-    	for (int i=1;i<res.size();i++){
-    		root->left=NULL;root->right=res[i];
-    		root=root->right;
-    	}
-    	root->left=NULL;root->right=NULL;
-    }
-    
-    void pregenerateTreeNode(TreeNode* root){
-    	if (root==NULL){
-    		return;
-    	}
-    	res.push_back(root);
-    	pregenerateTreeNode(root->left);
-    	pregenerateTreeNode(root->right);
+    	flatten(root->right);
+    	flatten(root->left);
+    	root->right = prev;
+    	root->left = NULL;
+    	prev = root;
     }
 };
-//这条题目有问题，上面的答案能够通过！！！？
+class Solution2 {
+public:
+    void flatten(TreeNode *root) {
+        TreeNode*now = root;
+        while (now)
+        {
+            if(now->left)
+            {
+                //Find current node's prenode that links to current node's right subtree
+                TreeNode* pre = now->left;
+                while(pre->right)
+                {
+                    pre = pre->right;
+                }
+                pre->right = now->right;
+                //Use current node's left subtree to replace its right subtree(original right 
+                //subtree is already linked by current node's prenode
+                now->right = now->left;
+                now->left = NULL;
+            }
+            now = now->right;
+        }
+    }
+};
+//第一题没问题，第二题还不会啊
