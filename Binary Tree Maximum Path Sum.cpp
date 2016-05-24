32e526ca99a6bcb4d79b17290ab9466f4bf40f8e
Jean <j.z.feng@foxmail.com>
2016-05-24 22:20:05 +0800

    Update Binary Tree Maximum Path Sum.cpp

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -31,7 +31,7 @@ public:
 //  这里不能写成int maxPathSumHelper(TreeNode* root,int sum) 
 //而 int sum应该是全局变量，如果写成局部变量，每次都要和左边节点的值比较
 //而如果是全局变量，就是每次更新sum值，  所以看solution2
-class Solution {
+class Solution2 {
 public:
     int res = INT_MIN;
     int maxPathSum(TreeNode* root) {
@@ -52,3 +52,31 @@ public:
     	return root->val + max(left, right);
     }
 };
+
+/*
+    int res = INT_MIN;
+    int maxPathSum(TreeNode* root) {
+        if (root->left == NULL&&root->right == NULL) {
+    		res = max(res, root->val);
+    		return res;
+    	}
+    	maxHelper(root);
+    	return res;
+    }
+    
+    int maxHelper(TreeNode* root) {
+    	if (root == NULL)
+    		return 0;
+    	if (root->left == NULL&&root->right == NULL) {
+    		//res = max(res, root->val);
+    		return root->val;
+    	}
+    	int left = max(0, maxHelper(root->left));
+    	int right = max(maxHelper(root->right), 0);
+    	res = max(res, left + right + root->val);
+    	return root->val + max(left, right);
+    }
+    本来 [0]就会导致程序出错，我在最开头 if (root->left == NULL&&root->right == NULL)   就是为了去除这种情况
+    另外就是判断有哪些可能性是不符合这个要求的 [-2,1]就是其中一个啊。
+    看了还是要验证res的最大值啊
+*/
