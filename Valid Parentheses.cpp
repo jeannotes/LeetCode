58c228b51c269b1d48859c73a647b3104320e366
Jean <j.z.feng@foxmail.com>
2016-06-03 08:27:49 +0800

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -31,3 +31,28 @@ public:
 //本题颇有难度，	if((c=='('&&s[i]!=')')||(c=='['&&s[i]!=']')||(c=='{'&&s[i]!='}'))  这个判断不曾想到
 //还有最后的 “((”的情况 也不曾想到
 //还有最后的 “((”的情况 也不曾想到
+bool isValid(string s) {
+	if (s.empty())
+		return true;
+	int len = s.size();
+	stack<char >stk;
+	for (int i = 0; i < len;i++) {
+		if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+			stk.push(s[i]);
+		else {
+			if (stk.empty())
+				return false;
+			char c = stk.top(); stk.pop();
+			if (c == '('&&s[i]==')')
+				continue;
+			if (c == '['&&s[i] == ']')
+				continue;
+			if (c == '{'&&s[i] == '}')
+				continue;
+			else
+				return false;
+		}
+	}
+	return stk.empty();
+}
+// 其实最后 想到 return stk.empty(); 是灵光乍现啊
