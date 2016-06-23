e6a81ec85035c90c775ad8b8bb1df9dec3826c48
Jean <j.z.feng@foxmail.com>
2016-06-23 18:21:01 +0800

    Update Populating Next Right Pointers in Each Node.cpp

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-		if (root==NULL){
-			return;
-		}
-		if (root->left){
-			root->left->next=root->right;
-		}
-		if (root->right&&root->next){
-			root->right->next=root->next->left;
-		}
-		connect(root->left);connect(root->right);
+	if (root==NULL){
+		return;
+	}
+	if (root->left){
+		root->left->next=root->right;
+	}
+	if (root->right&&root->next){
+		root->right->next=root->next->left;
+	}
+	connect(root->left);connect(root->right);
 	}
 };
 //值得庆祝的一天，递归写出来了
@@ -20,4 +20,4 @@ public:
 //第二种方法：if (root->right&&root->next) 按照道理不写左边的root->right没事，
 //因为第二句root->left存在，那么root->right一定存在
 //但是到最底层，最左边的时候，root->next是存在的，只是当前的root->right->next不存在
-// 这个是会的，但是后面一题就不会了
+// 感觉还可以啊
