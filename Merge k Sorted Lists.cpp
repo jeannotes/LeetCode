0cd04d436d7e6782e5123ea26df55f764c6d3e18
Jean <j.z.feng@foxmail.com>
2016-07-01 07:58:36 +0800

    Update Merge k Sorted Lists.cpp

--- Merge k Sorted Lists.cpp
+++ Merge k Sorted Lists.cpp
@@ -39,7 +39,7 @@ public:
 };
 // 要抽时间学习STL基本知识  
 //不会啊
-// operator 里面不需要使用 &
+// operator 里面不需要使用 &， 再次强调一下，operator 里面不需要使用 &
 /*
 priority_queue<ListNode *,vector<ListNode *>,Comp>pq;   ----  bool operator()(const ListNode i1, const ListNode *i2)
   这里面一定要统一 ，因为优先队列就是按照指针来的
