d57dbd5a70b94a8cffca1f97046cde975b86fc05
Jean <j.z.feng@foxmail.com>
2016-07-05 08:26:13 +0800

    Update Flatten Binary Tree to Linked List.cpp

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -11,29 +11,24 @@ public:
     	prev = root;
     }
 };
-class Solution2 {
-public:
-    void flatten(TreeNode *root) {
-        TreeNode*now = root;
-        while (now)
-        {
-            if(now->left)
-            {
-                //Find current node's prenode that links to current node's right subtree
-                TreeNode* pre = now->left;
-                while(pre->right)
-                {
-                    pre = pre->right;
-                }
-                pre->right = now->right;
-                //Use current node's left subtree to replace its right subtree(original right 
-                //subtree is already linked by current node's prenode
-                now->right = now->left;
-                now->left = NULL;
-            }
-            now = now->right;
-        }
-    }
-};
+
 //第一题没问题，第二题还不会啊
 //  递归的力量相当强大啊，其实他们的解法本质上是一样的。
+void flatten(TreeNode* root) {
+	if (root == NULL)
+		return;
+	while (root){
+		if (root->left) {//Find current node's prenode that links to current node's right subtree
+			TreeNode *pre = root->left;
+			while (pre->right){
+				pre = pre->right;
+			}
+			pre->right = root->right;
+			//Use current node's left subtree to replace its right subtree(original right 
+            //subtree is already linked by current node's prenode
+			root->right = root->left;
+			root->left = NULL;
+		}
+		root = root->right;
+	}
+}
