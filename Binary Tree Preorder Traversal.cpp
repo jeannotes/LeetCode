f10a466f818ad808aed94a86fe68bf4db46e492b
Jean <j.z.feng@foxmail.com>
2016-08-07 00:56:53 +0800

    Update Binary Tree Preorder Traversal.cpp

--- Binary Tree Preorder Traversal.cpp
+++ Binary Tree Preorder Traversal.cpp
@@ -23,26 +23,27 @@ public:
 //迭代版本，可能我这个是最简单的吧
 //一大早写了一个，非常方便，直接AC，非常开心
 //BTW，这是第二个修改版本
-class Solution2 {
-public:
-    vector<int>res;
-    vector<int> preorderTraversal(TreeNode* root) {
-    	if (root == NULL)
-    		return res;
-    	stack<TreeNode*>stk;
-    	while (root||!stk.empty()){
-    		while (root){
-    			stk.push(root); 
-    			res.push_back(root->val);
-    			root = root->left;
-    		}
-    		root = stk.top()->right; 
-    		stk.pop();
-    	}
-    	return res;
-    }
-};
+vector<int>res;
+vector<int> preorderTraversal(TreeNode* root) {
+	if (root == NULL)
+		return res;
+	stack<TreeNode*>s;
+
+	while (root || !s.empty()) {
+		if (root) {
+			s.push(root); 
+			res.push_back(root->val);
+			root = root->left;
+		}
+		else {
+			root = s.top();
+			s.pop();
+			root = root->right;
+		}
+	}
+	return res;
+}
 //非递归不会啊，不会啊
 //https://leetcode.com/discuss/100858/preorder-inorder-postorder-iterative-solution-by-c
 //  要么就if else  不然就只能是两个while语句啦，加油啊
-//c擦，问题还是很大
+//c擦，问题还是很大，更新下，这个版本非常好
