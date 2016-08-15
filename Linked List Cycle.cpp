259730f6f528fc2be488a8590efe8f87e4e27ac1
Jean <j.z.feng@foxmail.com>
2016-08-15 10:09:50 +0800

    Update Linked List Cycle.cpp

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -3,21 +3,20 @@ public:
     //https://leetcode.com/problems/linked-list-cycle/
     //待会说明下，刚刚那题还有一种n平方解法
     //http://blog.sina.com.cn/s/blog_6f611c300101fs1l.html
-    bool hasCycle(ListNode *head) {
-        ListNode *slow=head,*fast=head;
-		if (head==NULL||head->next==NULL){
+bool hasCycle(ListNode *head) {
+	if (head == NULL || head->next == NULL)
+		return false;
+	ListNode* fast = head, *slow = head;
+	while (fast&&fast->next) {
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == NULL)
 			return false;
-		}
-
-		while (fast!=NULL&&fast->next!=NULL){
-			fast=fast->next->next;slow=slow->next;
-			if (fast==NULL){
-				return false;
-			}
-			if(fast==slow)
-			    return true;
-		}
-    }
+		if (fast == slow)
+			return true;
+	}
+	return false;
+}
     //一开始程序出错在在一开始的if语句中判断了head->next->next==NULL，
     //如果只有两个链表，可以相互链接嘛，就像一个嘴的形状一样。
     //后来程序出错在没有写成fast=fast->next->next;而写的是fast=fast->next;
