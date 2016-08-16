fae1eda59b9062c4f30697014bb7736b3b45dcd8
Jean <j.z.feng@foxmail.com>
2016-08-16 10:51:08 +0800

    Update Linked List Cycle.cpp

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -20,5 +20,5 @@ bool hasCycle(ListNode *head) {
     //一开始程序出错在在一开始的if语句中判断了head->next->next==NULL，
     //如果只有两个链表，可以相互链接嘛，就像一个嘴的形状一样。
     //后来程序出错在没有写成fast=fast->next->next;而写的是fast=fast->next;
-    //再后来是while循环里面条件判断没有成功，只设立了一个if (fast==NULL) 加油！！
+    //再后来是while循环里面条件判断没有成功，只设立了一个if (fast==NULL) 加油！！，可以啊
 };
