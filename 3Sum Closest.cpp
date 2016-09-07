87e6919085b73ffce8e13ebc370f74d97fbd865e
Jean <j.z.feng@foxmail.com>
2016-09-07 23:27:16 +0800

    Update 3Sum Closest.cpp

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -20,7 +20,7 @@ public:
                     if(sum>target)
                         right--;
                     if(sum<target)
-                        left++;// 这部分不能放在 上面if的循环里面  DDD
+                        left++;// 这部分不能放在 上面if的循环里面  DDD  还行啊
                 }
             }
         }
