69476ab00f4bc711b3d83389ea79d9c74ce4ccfa
Jean <j.z.feng@foxmail.com>
2016-09-22 18:21:11 +0800

    Update Maximum Subarray.cpp

--- Maximum Subarray.cpp
+++ Maximum Subarray.cpp
@@ -48,7 +48,7 @@ public:
 //  假设前面k个是sum，第k+1个的时候，如果sum小于0，对我后面计算是没有好处的，但是前提是之前的最大和已经保存在maxSum里面啦。
 //不会啊，真的是好题目,好题目 至今不会啊，意思是如果第k个数字与前面的和是小于0的，那么就没有意义，则从头再来。 还是不会 
 
-//最大子矩阵和 
+//最大子矩阵和  good idea
 int max_subarray(vector<int> &array) {
     int res = 0, sum = 0;
     for (int i = 0; i < array.size(); ++i) {
