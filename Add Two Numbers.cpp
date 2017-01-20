52a9d8802080441693ea1d8f2516876b876e1804
Jean <j.z.feng@foxmail.com>
2017-01-20 11:25:02 +0800

    Update Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -35,3 +35,31 @@ public:
     }
 };
 //1
+
+//python
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self,l1,l2):
+        dummy=ListNode(0);
+        current,carry=dummy,0
+
+        while l1 or l2:
+            val=carry
+            if l1:
+                val+=l1.val
+                l1=l1.next
+            if l2:
+                val+=l2.val
+                l2=l2.next
+            current.next=ListNode(val%10);
+            carry=val/10;
+            current=current.next;
+            
+        if carry == 1:
+            current.next = ListNode(carry);
+        return  dummy.next
