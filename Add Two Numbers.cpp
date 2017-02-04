393610e3ad5beb82c346f069f20d16ffaaff15de
Jean <j.z.feng@foxmail.com>
2017-02-04 15:28:31 +0800

    Update Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -37,29 +37,34 @@ public:
 //1
 
 //python
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self,x):
+        self.x=x
+        self.next=None
 
 class Solution(object):
     def addTwoNumbers(self,l1,l2):
         dummy=ListNode(0);
-        current,carry=dummy,0
+        current,carry = dummy,0
 
-        while l1 or l2:
-            val=carry
-            if l1:
-                val+=l1.val
+        while l1 or l2 :
+            val = carry
+            if l1 :
+                val+=l1.x
                 l1=l1.next
-            if l2:
-                val+=l2.val
+            if l2 :
+                val +=l2.x
                 l2=l2.next
-            current.next=ListNode(val%10);
-            carry=val/10;
-            current=current.next;
-            
-        if carry == 1:
-            current.next = ListNode(carry);
+            carry,val = val/10 , val%10
+            current.next= ListNode(val)
+            current=current.next
+
+        if carry :
+            current.next = ListNode(carry)
         return  dummy.next
+
+if __name__ == '__main__':
+    a,a.next,a.next.next = ListNode(2),ListNode(4),ListNode(3)
+    b,b.next,b.next.next = ListNode(5),ListNode(6),ListNode(4)
+    result = Solution().addTwoNumbers(a,b)
+    print "{0}->{1}->{2}".format(result.x,result.next.x,result.next.next.x)
