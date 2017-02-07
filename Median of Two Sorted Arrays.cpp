ffe1c6fd7f628ac33d81fb709bd46730c69b18a6
Jean <j.z.feng@foxmail.com>
2017-02-08 00:16:02 +0800

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -36,3 +36,25 @@ public:
     }
 };
 // 好久没有做这题了
+class Solution:
+    def getKth(self,A,B,k):
+        m, n = len(A), len(B)
+        if m > n:
+            return self.getKth(B, A, k)
+        if m == 0:
+            return B[k - 1]
+        if k == 1:
+            return min(A[0], B[0])
+        pa=min(k/2,m)
+        pb=k-pa
+        if A[pa-1]<=B[pb-1]:
+            return self.getKth(A[pa:],B,pb)
+        else:
+            return self.getKth(A,B[pb:],pa)
+    
+    def findMedianSortedArrays(self,A,B):
+        m,n=len(A),len(B)
+        if (m+n)%2==1:
+            return self.getKth(A,B,(m+n)/2+1)
+        else:
+            return (self.getKth(A,B,(m+n)/2)+self.getKth(A,B,(m+n)/2+1))*0.5
