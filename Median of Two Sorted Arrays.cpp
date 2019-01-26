273e9f3f09e95e203b09a81fb363f3d57cef35d2
Jean <j.z.feng@foxmail.com>
2019-01-26 23:27:17 +0800

    Update Median of Two Sorted Arrays.cpp

--- Median of Two Sorted Arrays.cpp
+++ Median of Two Sorted Arrays.cpp
@@ -1,40 +1,51 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len=nums1.size()+nums2.size();
-        
-        if(len%2)//奇数
-            return  findKthElement( nums1 ,nums2 , (len+1)/2 ) ;
-        else//偶数    
-            return  (findKthElement(nums1,nums2,len/2)+findKthElement( nums1 , nums2 , len/2+1 ))*0.5 ;
+double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+{
+    int len = nums1.size() + nums2.size();
+    if (len % 2)
+    { // even
+        // 1 2 3 4
+        return findKthElement(nums1, nums2, (len + 1) / 2);
     }
-    
-    double min(double x,double y){
-        return x<y?x:y;
+    else
+    {
+        // 1 2 3
+        return (findKthElement(nums1, nums2, len / 2) + findKthElement(nums1, nums2, 1 + len / 2)) / 2;
     }
-    
-    double findKthElement(vector<int>& nums1,vector<int>& nums2,int k){
-        if(nums1.size()>nums2.size()){
-            return findKthElement(nums2,nums1,k);
-        }
-        
-        if(nums1.empty())   return nums2[k-1];
-        if(nums2.empty())   return nums1[k-1];
-        
-        if(k==1)    return min(nums1[0],nums2[0]);
-        
-        int ia=min(k/2,nums1.size());
-        int ib=k-ia;
-        if(nums1[ia-1]<nums2[ib-1]){
-            vector<int> new_nums1(nums1.begin()+ia,nums1.end());
-            return findKthElement( new_nums1 , nums2 , k-ia ) ;
-        }if(nums1[ia-1]>nums2[ib-1]){
-            vector<int> new_nums2(nums2.begin()+ib,nums2.end());
-            return findKthElement( nums1 , new_nums2 , k-ib ) ;
-        }
-        return nums1[ia-1];
+}
+
+double min(int x, int y)
+{
+    return x < y ? x : y;
+}
+
+double findKthElement(vector<int> &nums1, vector<int> &nums2, int k)
+{
+    if (nums1.size() > nums2.size())
+        return findKthElement(nums2, nums1, k);
+    if (nums1.empty())
+        return nums2[k - 1];
+    if (nums2.empty())
+        return nums1[k - 1];
+    if (k == 1)
+        return min(nums1[0], nums2[0]);
+    int ia = min(k / 2, nums1.size());
+    int ib = k - ia;
+    if (nums1[ia - 1] < nums2[ib - 1])
+    {
+        vector<int> new_vec(nums1.begin() + ia, nums1.end());
+        return findKthElement(new_vec, nums2, k - ia);
     }
-};
+    if (nums2[ib - 1] < nums1[ia - 1])
+    {
+        vector<int> new_vec(nums2.begin() + ib, nums2.end());
+        return findKthElement(nums1, new_vec, k - ib);
+    }
+    return nums1[ia - 1];
+}
+}
+;
 // 好久没有做这题了  怒做python
 class Solution:
     def getKth(self,A,B,k):
