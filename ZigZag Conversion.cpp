35a119755cd1f4084d8c55031a079dc867bc4da8
Jean <j.z.feng@foxmail.com>
2019-01-28 23:59:26 +0800

    Update ZigZag Conversion.cpp

--- ZigZag Conversion.cpp
+++ ZigZag Conversion.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int len=s.length();
-        if(numRows==1||len<=numRows){//还没有numRows大，直接从上往下读
-            return s;
-        }
-        string result;
-        int gap=2*numRows-2;
-        for(int i=0;i<numRows;i++){
-            for(int j=i;j<len;j=j+gap){
-                result.push_back(s[j]);
-                if(i>0&&i<numRows-1&&(j+gap-2*i)<len){
-                    result.push_back(s[j+gap-2*i]);
-                }
-                //除去第一行和最后一行，第二列之字形的数字
+        if(s.size()<=2) return s;
+        if(numRows == 1) return s; 
+        vector<string> strings(numRows);
+        for(int i=0;i<s.size();i++){
+            int len = i%(2*numRows-2);
+            if(len<= numRows-1){
+                strings[len]+=s[i]; 
+            }else{
+                int temp =(2*numRows-2)-len;
+                strings[temp]+=s[i]; 
             }
         }
+        string res;
+        for(auto s : strings){
+            res=res+s;
+        }
         
-        return result;
+        return res;
     }
 };
