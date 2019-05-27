5330d89b4e2fca2aaa1167a23fb315ca382d6899
Jean <j.z.feng@foxmail.com>
2019-05-27 20:44:43 +0800

    Update 3. Longest Substring Without Repeating Characters.cpp

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.empty()||s.size()==0)
-            return 0;
-        map<char,int>map;
-        int res=0;
-        // a b c a
-        for(int i=0,j=0;i<s.size();i++){
-            if(map.find(s[i])!=map.end()){
-                j=max(j,map[s[i]]+1);
-/*
-abba, j means next first single character
-*/
-                // j= map[s[i]]+1 ; //false code
+        if(s.empty()) return 0;
+        int len = s.size();
+        std::set<char> set;
+        int i=0,j=0;
+        int res = 0;
+        while(i<len &&j<len){
+            if(set.count(s[j]) ==0 ){
+                set.insert(s[j++]);
+                res = std::max(res, j-i);
+            }else{
+                set.erase(s[i++]);
             }
-            map[s[i]]=i;
-            res=max(res,i-j+1);
         }
+        
         return res;
     }
 };
 /*
-更正，这里，如果不使用ｍａｘ比较ｊ，那么就会把过去的某一个值给考虑进去了．比如说第一个ａ．
-从一开始，累计到第二个ｂ，这个没有问题，但是后面再次遇到ａ，那么就会设置ｊ＝１，此时发生错误
-example abba,很容易设置j=1.
-2. 为什么 j=max(j,map[s[i]]+1);而不是j=max(j,map[s[i]]);
-同样用abba的列子来说，abba，遇到第一个b，没问题，往下走，第二个b，那么就应该继续去看b的下一位，
-而不是当前，因为后面计算最长的时候是根据后面的“ba”来计算，而不是bba来计算。这里很容易出错
+这个方法其实非常好，解释是这样的，只要没有遇到相同的字符，那么不断的增加字符到set
+如果有，那么把最一开始的删除掉，这里其实已经说明了，过去的一段字符串也已经考虑到了。所以不用担心，
+我们真正关心的就是（abba）里面（bba），从第一个b开始，是不是有最大的。而代码里面把最一开始的给删除掉就是为了这个。
 */
