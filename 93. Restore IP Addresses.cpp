0bb76374ea01ea1d808e951d177f731dc42f25c6
Jean <j.z.feng@foxmail.com>
2019-07-07 14:34:52 +0800

    Update 93. Restore IP Addresses.cpp

--- 93. Restore IP Addresses.cpp
+++ 93. Restore IP Addresses.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-string res1;
-vector<string> res;
-vector<string> restoreIpAddresses(string s) {
-	if (s.empty() || s.size() > 12)
-		return res;
-	restoreIpAddressesHelper(s, 1, 0);
-	return res;
-}
-
-void restoreIpAddressesHelper(string s,int index,int start) {
-	if (index == 4) {
-		string ip = s.substr(start);
-		if (isValid(ip)) {
-			res1 += ip;
-			res.push_back(res1);
-			return;
-		}
-	}
-	for (int i = 1; i <= 3 && (s.size()-start-i>=4-index);i++) {
-		string ip = s.substr(start, i);
-		if (isValid(ip)) {
-			int len = res1.size();
-			res1 += ip; res1 += '.';
-			restoreIpAddressesHelper(s, index + 1, start + i);
-			res1.resize(len);
-		}
-	}
-}
-
-bool isValid(string s) {
-    if (s.size() > 1 && s[0] == '0')
-		return false;
-	int tem = stoi(s);
-	return 0 <= tem&&tem <= 255;
-}
+    vector<string> res;
+    string temp;
+    vector<string> restoreIpAddresses(string s) {
+        if (s.empty() || s.size() > 12)
+		    return res;
+        restoreHelper(s, 1, 0);
+        return res;
+    }
+    
+    void restoreHelper(string s, int index, int start){
+        if(index == 4){
+            string ip = s.substr(start);
+            if(isValid(ip)){
+                temp+=ip;
+                res.push_back(temp);
+            }
+        }
+        
+        for(int i=1;i<4 && s.size()-i-start >=4-index ;i++){
+            string ip = s.substr(start, i);
+            if(isValid(ip)){
+                int len = temp.size();
+                temp+=ip;
+                temp+='.';
+                restoreHelper(s, index+1, start+i);
+                temp.resize(len);
+            }
+        }
+    }
+    
+    bool isValid(string s){
+        if(s.empty()) return false;
+        if(s.size()>1 && s[0]=='0') return false;
+        int num = stoi(s);
+        return 0<=num && num<=255;
+    }
 };
 //总算是能够理解一丢丢
 // 想了好久，仅仅知道i=1,2,3,第二天早上做的，有些会了
