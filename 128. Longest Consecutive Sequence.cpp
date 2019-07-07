05aa4eb99987971dfb662590ad77176c8cf1b3b0
Jean <j.z.feng@foxmail.com>
2019-07-07 08:42:24 +0800

    Update and rename Longest Consecutive Sequence.cpp to 128. Longest Consecutive Sequence.cpp

---
+++ 128. Longest Consecutive Sequence.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+		unordered_set<int>mp;
+		for (int i=0;i<nums.size();i++){
+			mp.insert(nums[i]);
+		}
+		int res=0;
+		for (int i=0;i<nums.size();i++){
+			int count=1;
+			for (int j=nums[i]+1;mp.find(j)!=mp.end();j++){
+				count++;mp.erase(j);
+			}
+			for (int j=nums[i]-1;mp.find(j)!=mp.end();j--){
+				count++;mp.erase(j);
+			}res=max(res,count);
+			if(mp.size()<1)
+			    break;
+		}
+		return res;
+	}
+};
+// 丫丫，小错误不断啊 yanse bianhua a 
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        sort(nums.begin(),nums.end());
+        int cur=1,res=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                if(nums[i]==nums[i-1]+1){
+                    cur+=1;
+                }else{
+                    res = max(res, cur);
+                    cur = 1;
+                }
+            }
+        }
+        res = max(res, cur);
+        return res;
+    }
+};
