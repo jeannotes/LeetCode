36ac54274555b2cb535637c375b78e7a40d36b52
Jean <j.z.feng@foxmail.com>
2019-07-11 20:36:33 +0800

    Update and rename Find Minimum in Rotated Sorted Array.cpp to 153. Find Minimum in Rotated Sorted Array.cpp

---
+++ 153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+         if(nums.size()==1) return nums[0];
+        
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]) continue;
+            if(nums[i]>nums[i+1]) return nums[i+1];
+        }
+        // 1 2 3 3
+        return nums[0];
+    }
+};
