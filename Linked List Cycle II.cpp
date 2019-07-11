e213de9678a3e61c89fefc414c3eea60148bf7dc
Jean <j.z.feng@foxmail.com>
2019-07-11 22:00:56 +0800

    Update Linked List Cycle II.cpp

--- Linked List Cycle II.cpp
+++ Linked List Cycle II.cpp
@@ -1,41 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
 class Solution {
 public:
-    //https://leetcode.com/problems/linked-list-cycle/
-    //  http://www.cnblogs.com/hiddenfox/p/3408931.html
     ListNode *detectCycle(ListNode *head) {
-        ListNode *slow=head,*fast=head;
-		if (head==NULL||head->next==NULL){
-			return NULL;
-		}
-
-		while (fast!=NULL&&fast->next!=NULL){
-			fast=fast->next->next;slow=slow->next;
-			if (fast==NULL){
-				return NULL;
-			}
-			if(fast==slow){
-			    fast=head;
-		        while(fast!=slow){
-    		    fast=fast->next;
-    		    slow=slow->next;
-    		    }
-		        return fast;
-			}
-		}
-		return NULL;
+        if(!head || !head->next) return NULL;
+        
+        ListNode *fast = head, *slow=head;
+    //  http://www.cnblogs.com/hiddenfox/p/3408931.html        
+        while(fast&&fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow){
+                slow = head;
+                
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return fast;
+            }
+        }
+        
+        return NULL;
     }
     //在while循环里面的if函数没有添加fast->next==NULL，看来前面一个程序也是要重新跑一下。
     //问题出在fast此时是最后一个节点，本身不为NULL，但是fast->next=NULL，所以不会进入循环
     //相反会执行下面的程序。 今天 今天下午笔试 
     //好题目啊，很好的题目
-    //函数必须要有输出啊，一直把最后的输出弄成return fast了
+    //函数必须要有输出啊，一直把最后的输出弄成return fast了	
 };
