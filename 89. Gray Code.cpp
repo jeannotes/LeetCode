afbfb514bfc93df87fff77a8142fc9f679837075
Jean <j.z.feng@foxmail.com>
2019-07-13 10:06:13 +0800

    Update 89. Gray Code.cpp

--- 89. Gray Code.cpp
+++ 89. Gray Code.cpp
@@ -4,7 +4,7 @@ public:
 		vector<int> res;
 		res.push_back(0);
 		for (int i=1;i<=n;i++){
-			int j=1<<(i-1);
+			int j=1<<(i-1);// not i
 			for (int k=res.size()-1;k>=0;k--){
 				res.push_back(res[k]+j);
 			}
@@ -12,7 +12,3 @@ public:
 		return res;
 	}
 };
-//大神还是很多的，代码就是好看
-//  int j=1<<(i-1);  这个才是乘以2啊，加油
-//  首先写的不顺利 还有就是 1<<(i-1)  写成  2<<(i-1) 
-//  首先写的不顺利 还有就是 1<<(i-1)  写成  2<<(i-1)   还是这个老毛病啊
