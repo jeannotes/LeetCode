4c19522022f6143cb20fe18cdeb2117d2317a989
Jean <j.z.feng@foxmail.com>
2019-07-14 09:07:02 +0800

    Update and rename Combinations.cpp to 77. Combinations.cpp

--- 77. Combinations.cpp
+++ 77. Combinations.cpp
@@ -27,24 +27,29 @@ public:
 };
 //不会
 // if(start>n)  这句话没有判断
-vector<vector<int>>res; vector<int>tem;
-vector<vector<int>> combine(int n, int k) {
-	combineHelper(1, n, k, 0);
-	return res;
-}
-
-void combineHelper(int start, int n,int k,int t) {
-	if (t == k) {
-		res.push_back(tem); return;
-	}
-	for (int i = start; i <= n;i++) {
-		tem.push_back(i);
-		combineHelper(i + 1, n, k, t + 1);
-		tem.pop_back();
-	}
-}
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int>temp;
+    vector<vector<int>> combine(int n, int k) {
+        if(n==0) return res;
+        combineHelper(n,k,1,0);
+        
+        return res;
+    }
+    
+    void combineHelper(int n, int k, int start,int index){
+        if(index == k){
+            res.push_back(temp);
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            temp.push_back(i);
+            combineHelper(n,k,i+1,index+1);
+            temp.pop_back();
+        }
+    }
+};
 //我的另外一个解法
-// 没问题，只是没有能够很快解决
-//  今天早上的问题在于，苏小妍考到这题
 // 可以的，首先 我是能够得到[1 2],我担心的是不能得到[1 3]
 //但是 其实是可以的，因为最后 ..helper(i+1,..) 这本身就会有好几种情况，他自身也是循环，也存在[2] [3]等等
