d29aa3a8fc5886736c31daf118b22138ef5d4028
Jean <j.z.feng@foxmail.com>
2019-07-21 18:42:59 +0800

    Update 26. Remove Duplicates from Sorted Array.cpp

--- 26. Remove Duplicates from Sorted Array.cpp
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty())
-            return 0;
-        int len=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[len]){
-                nums[++len]=nums[i];
-            }
+        int len = 0;
+        for(int i=0;i<nums.size();i++){
+            if(len<1 || nums[i]>nums[len-1])
+                nums[len++] = nums[i];
         }
-        return len+1;
-        //对比remove element，这里可以这样思考
-        //如果全部为1，一开始len是0，最后仅仅保留一个自然要+1输出
-        //这一次错误在于里面的语句写成nums[len++]=nums[i]
+        return len;
     }
 };
 //一次通过
