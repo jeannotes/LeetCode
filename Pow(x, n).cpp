d2dc4295b00428f8ab7e3e4ee3f9a5db949d9eb7
Jean <j.z.feng@foxmail.com>
2022-05-31 12:27:44 +0800

    Update Pow(x, n).cpp

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -1,16 +1,23 @@
-double myPow(double x, int n) {
-	if (x == 0||n==0)
-		return 1;
-	if (n < 0) {
-		if (n == INT_MIN)
-			return 1.0/(myPow(x,INT_MAX)*x);
-		return myPow(1.0 / x, -n);
-	}
-
-	if (n % 2)
-		return myPow(x*x, n / 2)*x;
-	return myPow(x*x, n / 2);
-}
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(n<0){
+            if(n==INT_MIN){
+                return myPow(1.0/x, INT_MAX)*1.0/x;
+            }
+            return myPow(1.0/x,-n);
+        }
+        
+        if(n%2)
+            return myPow(x*x,n/2)*x;
+        else
+            return myPow(x*x,n/2);
+        
+    }
+};
 //用位运算代替乘除和求余优化效率：移位代替除法（n>>1 == n /2);用位与代替求余判定奇偶（if (n& 0x1 == 1) == if(n % 2 == 1))
 
 //对于n取值INT_MIN = -2147483648时，-n依然是INT_MIN，并不是INT_MAX=2147483647，这时需要格外小心。
