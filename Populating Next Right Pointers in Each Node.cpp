be9e9c3348a70819d016e0d34a55ceb8b987ebf6
Jean <j.z.feng@foxmail.com>
2022-06-02 10:44:08 +0800

    Update Populating Next Right Pointers in Each Node.cpp

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -13,6 +13,34 @@ public:
 	connect(root->left);connect(root->right);
 	}
 };
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        Node *dummy=new Node(0), *dummy2=root;
+        
+        if(root==NULL)  return root;
+        
+        while(root!=NULL){
+            Node *tail=dummy;
+            while(root!=NULL){
+                if(root->left){
+                    tail->next=root->left;
+                    tail=tail->next;
+                }
+                if(root->right){
+                    tail->next=root->right;
+                    tail=tail->next;
+                }
+                root=root->next;
+            }
+            root=dummy->next;
+            dummy->next=NULL;//如果没有这句话，那么dummy只是上面那一层的
+        }
+        delete dummy;
+        return dummy2;
+    }
+};
 //值得庆祝的一天，递归写出来了
 
 //还是有许多问题啊
