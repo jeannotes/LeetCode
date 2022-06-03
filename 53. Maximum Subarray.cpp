7d0b70fa34572daee8ec69cb2a114c74e23bcf9d
Jean <j.z.feng@foxmail.com>
2022-06-03 13:41:00 +0800

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -7,7 +7,18 @@
         }
         return res;
     }
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res=nums[0];
+        vector<int> dp(nums.size(), res);
+        for(int i=1;i<nums.size();i++){
+            dp[i]=max(nums[i], dp[i-1]+nums[i]);
+            res=max(dp[i],res);
+        }
+        return res;
+    }
+};
 //  假设前面k个是sum，第k+1个的时候，如果sum小于0，对我后面计算是没有好处的，但是前提是之前的最大和已经保存在maxSum里面啦。
 //不会啊，真的是好题目,好题目 至今不会啊，意思是如果第k个数字与前面的和是小于0的，那么就没有意义，则从头再来。 还是不会 
 // good
