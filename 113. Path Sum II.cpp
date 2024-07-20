af6f5209cbde501e16b72b23854e1ea4f71bae5c
Jean <j.z.feng@foxmail.com>
2024-07-20 23:37:50 +0800

    Update 113. Path Sum II.cpp

--- 113. Path Sum II.cpp
+++ 113. Path Sum II.cpp
@@ -1,22 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<int>tem;
-    vector<vector<int>>res;
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-    	pathSumHelper(root, sum);
-    	return res;
+
+    vector<int> tem;
+    vector<vector<int>> res;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (root==NULL) return res;
+        pathSumHelper(root, 
+                       targetSum);
+        return res;
     }
-    
-    void pathSumHelper(TreeNode* root, int sum) {
-    	if (root == NULL)
-    		return;
-    	tem.push_back(root->val);
-    	if (root->left == NULL&&root->right == NULL&&root->val == sum) {
-    		res.push_back(tem);
-    	}
-    	pathSumHelper(root->left, sum - root->val);
-    	pathSumHelper(root->right, sum - root->val);
-    	tem.pop_back();
+    void pathSumHelper(TreeNode* root, 
+                        int targetSum) { 
+        if (root==NULL) return ;
+        tem.push_back(root->val);
+        if (root->left==NULL && root->right==NULL && root->val==targetSum){
+            res.push_back(tem);
+        }        
+        pathSumHelper(root->left, 
+                       targetSum-root->val);
+        pathSumHelper(root->right, 
+                       targetSum-root->val);
+        tem.pop_back();
     }
 };
 //其实我还是不会，中间的return 还是有问题啊
