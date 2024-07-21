172056ce4f8ce71f56a18a6ca305de6c46b49d55
Jean <j.z.feng@foxmail.com>
2024-07-21 19:41:57 +0800

    Update 55. Jump Game.cpp

--- 55. Jump Game.cpp
+++ 55. Jump Game.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int index=0;
-        int n=nums.size()-1;
-        for(int i=0;i<nums.size();i++){
-            if(index<i || index >=n) break;
-            index = max(index, i+nums[i]);
+        if(nums.size()<=1) return true;
+        int curmax=0, end=0, ans=0;
+        for(int i=0;i<nums.size()-1;i++){
+            curmax=max(curmax, nums[i]+i);
+            if(curmax<=i) return false;
+            if(i==end){
+                ans++;
+                end=curmax;
+            }
         }
-        
-        return (index>=n)?1:0;
+        return curmax >= nums.size()-1;
     }
 };
-//http://bangbingsyb.blogspot.com/2014/11/leetcode-jump-game-i-ii.html
+//https://blog.lichangao.com/daily_practice/leetcode/greedy/basis.html#_0055-%E8%B7%B3%E8%B7%83%E6%B8%B8%E6%88%8F
