66e2d63f4d367ab4eae799dcb784ef847646aaf3
Jean <j.z.feng@foxmail.com>
2024-07-22 13:41:05 +0800

    Update 64. Minimum Path Sum.cpp

--- 64. Minimum Path Sum.cpp
+++ 64. Minimum Path Sum.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        if(grid.empty()||grid[0].empty())
-            return 0;
+        if(grid.empty()||grid[0].empty()) return 0;
         int m=grid.size(),n=grid[0].size();
-        
-        for(int i=1;i<m;i++)
-            grid[i][0]+=grid[i-1][0];
-        for(int i=0;i<n;i++)
-            grid[0][i]+=grid[0][i-1];
-            
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                grid[i][j]=min(grid[i-1][j],grid[i][j-1])+grid[i][j];
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) {dp[i][j]=grid[i][j];continue;}
+                if(i) dp[i][j]=min(dp[i][j],dp[i-1][j]+grid[i][j]);
+                if(j) dp[i][j]=min(dp[i][j],dp[i][j-1]+grid[i][j]);
             }
         }
-        
-        return grid[m-1][n-1];
+        return dp[m-1][n-1];
     }
 };
 //bug-free
