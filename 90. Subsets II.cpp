bf94022b0493a0a5371a1f06e6502072d0dc1ca0
Jean <j.z.feng@foxmail.com>
2024-07-25 21:59:15 +0800

    Update 90. Subsets II.cpp

--- 90. Subsets II.cpp
+++ 90. Subsets II.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>>res;
-    vector<int>tem;
+    vector<vector<int>> res;
+    vector<int> tem;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        res.push_back(vector<int>());
-        subsetsHelper(0,nums);
+        res.push_back(tem);
+        subsetsHelper(nums, 0);
         return res;
     }
-    
-    void subsetsHelper(int start,vector<int>&nums){
-        if(start==nums.size())
-            return;
-        
-        for(int i=start;i<nums.size();i++){
-            if(i>start&&nums[i]==nums[i-1])// attention
-                continue;
-            tem.push_back(nums[i]);
-            res.push_back(tem);
-            subsetsHelper(i+1,nums);
-            tem.pop_back();
+    void subsetsHelper(vector<int>& nums, int start) {
+        if(start >= nums.size()) return;
+        unordered_set<int> u_st;
+        for(int i=start; i<nums.size(); i++){
+            if(u_st.find(nums[i]) == u_st.end()){
+                u_st.insert(nums[i]);
+                tem.push_back(nums[i]);
+                res.push_back(tem);
+                subsetsHelper(nums, i+1);
+                tem.pop_back();
+            }
+            
         }
     }
 };
-// 还行啊，就是一开始插入一个空的不太会了
-// 测试的时候心里很紧张啊
-// 还不错，小心谨慎啊
