a8f2d3640a0f6a415053ebd443b83eebed5d333a
Jean <j.z.feng@foxmail.com>
2024-07-29 22:26:38 +0800

    Update 72.Edit Distance.cpp

--- 72.Edit Distance.cpp
+++ 72.Edit Distance.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        int n=word1.size(), m=word2.size();
-        word1=' '+word1;
-        word2=' '+word2;
-        
-        vector<vector<int>>dp(n+1, vector<int>(m+1, INT_MAX));
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int j = 0; j <= m; j++) dp[0][j] = j;
-        for(int i=1; i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(word1[i]==word2[j]) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=min(dp[i-1][j-1], min(dp[i-1][j],dp[i][j-1]))+1;
+        int m=word1.size(), n=word2.size();
+        vector<vector<int>>dp(m+1, vector<int>(n+1, INT_MAX));
+
+        for(int i=0; i<=m; i++){
+            dp[i][0]=i;
+        }
+        for(int j=0; j<=n; j++){
+            dp[0][j]=j;
+        }
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1[i-1]!=word2[j-1]){
+                    dp[i][j]=min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1;
+                }
+                else
+                    dp[i][j] = dp[i-1][j-1];
             }
         }
-        return dp[n][m];
+        return dp[m][n];
     }
 };
-//不会，重新更新了一下，主要分成相等以及不相等的情况， 好题目 
 /*
 // 替换 删除 插入  把三个过程分开 就很简单了
 				dp[i][j]=dp[i-1][j-1]+1;//替换
