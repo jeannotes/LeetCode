22273b9cc2167e8bcb5a4341bb4da5bf24313cfe
Jean <j.z.feng@foxmail.com>
2024-08-01 21:20:19 +0800

    Update and rename Binary Tree Maximum Path Sum.cpp to 124.Binary Tree Maximum Path Sum.cpp

--- 124.Binary Tree Maximum Path Sum.cpp
+++ 124.Binary Tree Maximum Path Sum.cpp
@@ -31,51 +31,31 @@ public:
 //  这里不能写成int maxPathSumHelper(TreeNode* root,int sum) 
 //而 int sum应该是全局变量，如果写成局部变量，每次都要和左边节点的值比较
 //而如果是全局变量，就是每次更新sum值，  所以看solution2  还是不会的，会了一丢丢
-class Solution2 {
+class Solution {
 public:
-    int res = INT_MIN;
+    int sum = INT_MIN;
     int maxPathSum(TreeNode* root) {
-    	maxHelper(root);
-    	return res;
+        if(!root) return 0;
+        maxPathSumHelper(root);
+        return sum;
     }
-    
-    int maxHelper(TreeNode* root) {
-    	if (root == NULL)
-    		return 0;
-    	if (root->left == NULL&&root->right == NULL) {
-    		res = max(res, root->val);
-    		return root->val;
-    	}
-    	int left = max(0, maxHelper(root->left));
-    	int right = max(maxHelper(root->right), 0);
-    	res = max(res, left + right + root->val);
-    	return root->val + max(left, right);
+    int maxPathSumHelper(TreeNode* root) {
+        if(!root) return 0;
+        if(!root->left && !root->right) {
+            sum = max(sum, root->val);
+            return root->val;
+        }
+        
+
+        int left = max(0, maxPathSumHelper(root->left));
+        int right = max(0, maxPathSumHelper(root->right));
+
+        sum = max(sum, root->val+left+right);
+        return root->val + max(left, right); 
     }
 };
 
-/*
-    int res = INT_MIN;
-    int maxPathSum(TreeNode* root) {
-        if (root->left == NULL&&root->right == NULL) {
-    		res = max(res, root->val);
-    		return res;
-    	}
-    	maxHelper(root);
-    	return res;
-    }
-    
-    int maxHelper(TreeNode* root) {
-    	if (root == NULL)
-    		return 0;
-    	if (root->left == NULL&&root->right == NULL) {
-    		//res = max(res, root->val);
-    		return root->val;
-    	}
-    	int left = max(0, maxHelper(root->left));
-    	int right = max(maxHelper(root->right), 0);
-    	res = max(res, left + right + root->val);
-    	return root->val + max(left, right);
-    }
+/* 
     本来 [0]就会导致程序出错，我在最开头 if (root->left == NULL&&root->right == NULL)   就是为了去除这种情况
     另外就是判断有哪些可能性是不符合这个要求的 [-2,1]就是其中一个啊。
     看了还是要验证res的最大值啊
