6f7f2e8483583f84b16c4f84a90b3adc4fcb7c44
Jean <j.z.feng@foxmail.com>
2024-08-19 23:27:31 +0800

    Update and rename Linked List Cycle II.cpp to 142. Linked List Cycle II.cpp

--- 142. Linked List Cycle II.cpp
+++ 142. Linked List Cycle II.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {// https://programmercarl.com/0142.%E7%8E%AF%E5%BD%A2%E9%93%BE%E8%A1%A8II.html#%E6%80%9D%E8%B7%AF
 public:
     ListNode *detectCycle(ListNode *head) {
         if(!head || !head->next) return NULL;
