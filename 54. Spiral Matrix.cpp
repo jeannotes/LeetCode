64562f22052ea9c396e644166b8e8c04b6ab02d5
Jean <j.z.feng@foxmail.com>
2024-09-10 21:01:59 +0800

    Update 54. Spiral Matrix.cpp

--- 54. Spiral Matrix.cpp
+++ 54. Spiral Matrix.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>res;
-        if(matrix.empty()||matrix[0].empty())
-            return res;
-        int m=matrix.size(),n=matrix[0].size();
-        int left=0,right=n-1;
-        int up=0,down=m-1;
-        
-        while(up<=down&&left<=right){
-            for(int i=left;i<right;i++)
-                res.push_back(matrix[up][i]);
-            for(int i=up;i<=down;i++)
-                res.push_back(matrix[i][right]);
-            if(up==down||right==left)
-                break;
-            //这里不能和之前旋转矩阵一样，因为如果出现up=down，需要退出，
-            //所以必须i<=down
-            //didn't understand
-            for(int i=right-1;i>=left;i--)
-                res.push_back(matrix[down][i]);
-            for(int i=down-1;i>up;i--)
-                res.push_back(matrix[i][left]);
-            left++;
-            right--;
-            up++;
-            down--;    
+        vector<int> res;
+        int m=matrix.size(), n=matrix[0].size();
+
+        int left=0, right=n-1, up=0, down=m-1;
+        while(left<=right && up <=down){
+            for(int i=left; i<=right; i++) res.push_back(matrix[up][i]);
+            if(++up > down) break;
+            for(int i=up; i<=down; i++) res.push_back(matrix[i][right]);
+            if(--right < left) break;
+            for(int i=right; i>=left; i--) res.push_back(matrix[down][i]);
+            if(--down<up) break;
+            for(int i=down;i>=up;i--) res.push_back(matrix[i][left]);
+            if(left++>right) break;
         }
-        
         return res;
     }
 };
