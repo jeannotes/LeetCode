b3c08145f5a9fd033e98f50ed7bbbe36435f8c03
Jean <j.z.feng@foxmail.com>
2024-09-10 21:02:19 +0800

    Update 59. Spiral Matrix II.cpp

--- 59. Spiral Matrix II.cpp
+++ 59. Spiral Matrix II.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>matrix(n,vector<int>(n,0));
-        if(n==0)
-            return matrix;
-        int left=0,right=n-1,up=0,down=n-1;
-        int count=1;
-        while(up<=down&&left<=right){
-            for(int i=left;i<right;i++)
-                matrix[up][i]=count++;
-            for(int i=up;i<=down;i++)
-                matrix[i][right]=count++;
-            
-            if(up==down||right==left)
-                break;
-            //这里不能和之前旋转矩阵一样，因为如果出现up=down，需要退出，
-            //所以必须i<=down
-            for(int i=right-1;i>=left;i--)
-                matrix[down][i]=count++;
-            for(int i=down-1;i>up;i--)
-                matrix[i][left]=count++;
-            left++;
-            right--;
-            up++;
-            down--;    
+        vector<vector<int>> matrix(n, vector<int>(n,0));  
+        int m=n, count=1;
+
+        int left=0, right=n-1, up=0, down=m-1;
+        while(left<=right && up <=down){
+            for(int i=left; i<=right; i++) matrix[up][i]=count++;
+            if(++up > down) break;
+            for(int i=up; i<=down; i++) matrix[i][right]=count++;
+            if(--right < left) break;
+            for(int i=right; i>=left; i--) matrix[down][i]=count++;
+            if(--down<up) break;
+            for(int i=down;i>=up;i--) matrix[i][left]=count++;
+            if(left++>right) break;
         }
-        
         return matrix;
     }
 };
